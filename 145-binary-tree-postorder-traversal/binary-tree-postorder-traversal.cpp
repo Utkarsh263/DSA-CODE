0c17d3361d5e42190dbab590bb38285715cde953
Utkarsh <utkarshkohli821@gmail.com>
2025-10-04 23:21:05 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (92.31%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -1,30 +1,47 @@
-class Solution{
+// class Solution{
+// public:
+//     vector<int>postorderTraversal(TreeNode* root){
+//         vector<int> postorder;
+//         if(root == NULL){
+//             return postorder;
+//         }
+//         stack<TreeNode*> st1, st2;
+//         st1.push(root);
+//         while(!st1.empty()){
+//             root = st1.top();
+//             st1.pop();
+//             st2.push(root);
+
+//             if(root->left != NULL){
+//                 st1.push(root->left);
+//             }
+
+//             if(root->right != NULL){
+//                 st1.push(root->right);
+//             }
+//         }
+
+//         while(!st2.empty()){
+//             postorder.push_back(st2.top()->val);
+//             st2.pop();
+//         }
+//         return postorder;
+//     }
+// };
+
+class Solution {
 public:
-    vector<int>postorderTraversal(TreeNode* root){
-        vector<int> postorder;
-        if(root == NULL){
-            return postorder;
+    void postorder(TreeNode* root , vector<int> &ans){
+        if(!root){
+            return;
         }
-        stack<TreeNode*> st1, st2;
-        st1.push(root);
-        while(!st1.empty()){
-            root = st1.top();
-            st1.pop();
-            st2.push(root);
-
-            if(root->left != NULL){
-                st1.push(root->left);
-            }
-
-            if(root->right != NULL){
-                st1.push(root->right);
-            }
-        }
-
-        while(!st2.empty()){
-            postorder.push_back(st2.top()->val);
-            st2.pop();
-        }
-        return postorder;
+        postorder(root->left , ans);
+        postorder(root->right , ans);
+        ans.push_back(root->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        postorder(root, ans);
+        return ans;
     }
 };
