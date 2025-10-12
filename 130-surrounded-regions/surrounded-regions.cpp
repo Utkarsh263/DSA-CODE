47d2e20388b4a51e5a05f7cbd1193e7c69e46ebf
Utkarsh <utkarshkohli821@gmail.com>
2025-10-12 22:29:58 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (90.95%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    void dfs(vector<vector<char>>&board , int i, int j,int m , int n){
+        if(i<0 || j<0 || i>=m || j>=n || board[i][j] != 'O'){
+            return;
+        }
+
+        board[i][j]= 'S';
+        // Explore all four direction to safe adjacent border o's
+        dfs(board, i+1, j, m, n);
+        dfs(board, i-1, j, m, n);
+        dfs(board, i, j+1, m, n);
+        dfs(board, i, j-1, m, n);
+
+    }
+    void solve(vector<vector<char>>& board) {
+        if(board.empty()){
+            return ;
+        }
+        int m = board.size();
+        int n = board[0].size();
+
+        // Mark all bordered O as S denotes safe region 
+        for(int i=0; i<m; i++){
+            if(board[i][0]=='O'){
+                dfs(board,i,0,m,n);
+            }
+            if(board[i][n-1]=='O'){
+                dfs(board,i,n-1,m,n);
+            }
+        }
+
+        for(int j=0; j<n;j++){
+            if(board[0][j]=='O'){
+                dfs(board,0,j,m,n);
+            }
+            if(board[m-1][j]=='O'){
+                dfs(board,m-1,j,m,n);
+            }
+        }
+
+        // Flip after safing all bordered 'O'
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(board[i][j]=='O'){
+                    board[i][j]='X';
+                }else if(board[i][j]=='S'){
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+};
