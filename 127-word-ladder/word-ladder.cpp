b01f190e26723c443af3499e5f5a633f412fdbb8
Utkarsh <utkarshkohli821@gmail.com>
2025-10-12 23:52:21 +0530

    Time: 51 ms (81.03%) | Memory: 21.1 MB (63.81%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string>wordSet(wordList.begin(), wordList.end());
+        unordered_set<string> wordSet(wordList.begin(), wordList.end());
 
-        // if endword not in the list , no valid path
         if(wordSet.find(endWord)== wordSet.end()){
             return 0;
         }
-
-        queue<pair<string , int>>q;
-        q.push({beginWord, 1}); // {word, transformation length}
+        queue<pair<string, int>>q;
+        q.push({beginWord, 1});
 
         while(!q.empty()){
             auto[words, steps] = q.front();
@@ -19,18 +17,18 @@ public:
                 return steps;
             }
 
-            // try changing characters
             for(int i=0; i<words.size(); i++){
                 string temp= words;
-                for(char ch = 'a' ; ch <= 'z'; ch++){
+                for(char ch = 'a'; ch<='z'; ch++){
                     temp[i]= ch;
 
-                    // if new word found in word set
-                    if(wordSet.find(temp) != wordSet.end()){
-                        q.push({temp , steps+1});
-                        wordSet.erase(temp);//mark as visited
-                    }
+                     if(wordSet.find(temp) != wordSet.end()){
+                    q.push({temp, steps+1});
+                    wordSet.erase(temp);
                 }
+                }
+
+               
             }
         }
         return 0;
