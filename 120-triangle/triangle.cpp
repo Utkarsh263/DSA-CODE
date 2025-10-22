82d68f5c61017c7ad4f5d59b213e8411f35e578a
Utkarsh <utkarshkohli821@gmail.com>
2025-10-22 20:46:20 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (60.61%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,3 +1,29 @@
+// class Solution {
+// public:
+//     int solve(int i , int j , vector<vector<int>>&triangle, vector<vector<int>>&dp , int n){
+//         if(i==n-1){
+//             return triangle[i][j];
+//         }
+
+//         if(dp[i][j] != -1){
+//             return dp[i][j];
+//         }
+
+//         int down = triangle[i][j] + solve(i+1, j, triangle, dp, n);
+//         int diag = triangle[i][j] + solve(i+1, j+1 , triangle , dp, n);
+
+//         return dp[i][j]= min(down , diag);
+//     }
+//     int minimumTotal(vector<vector<int>>& triangle) {
+//         int n = triangle.size();
+//         vector<vector<int>>dp(n , vector<int>(n, -1));
+
+//         return solve(0, 0, triangle, dp, n);
+//     }
+// };
+
+
+
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
