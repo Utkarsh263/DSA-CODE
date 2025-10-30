a7e58e50ec869eb6d78e440421ccd857ef573c81
Utkarsh <utkarshkohli821@gmail.com>
2025-10-30 09:58:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (35.18%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -15,3 +15,26 @@ public:
         }
     }
 };
+
+// Brute Force approach 
+// class Solution {
+// public:
+//     void rotate(vector<vector<int>>& matrix) {
+//         int n = matrix.size();
+//         vector<vector<int>> temp(n, vector<int>(n));
+
+//         // Place elements in rotated positions
+//         for(int i = 0; i < n; i++){
+//             for(int j = 0; j < n; j++){
+//                 temp[j][n - 1 - i] = matrix[i][j];
+//             }
+//         }
+
+//         // Copy back to original matrix
+//         for(int i = 0; i < n; i++){
+//             for(int j = 0; j < n; j++){
+//                 matrix[i][j] = temp[i][j];
+//             }
+//         }
+//     }
+// };
