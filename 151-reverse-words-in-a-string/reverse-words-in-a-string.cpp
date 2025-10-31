acf5f97b5649d5dd3cdabbddb3904de11bb748e5
Utkarsh <utkarshkohli821@gmail.com>
2025-11-01 00:11:18 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (90.01%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,61 +1,65 @@
-class Solution {
-public:
-    string reverseWords(string s) {
-        stringstream ss(s);
-        string word;
-        vector<string> words;
-
-        while(ss >> word){
-            words.push_back(word);
-        }
-
-        reverse(words.begin(), words.end());
-
-        string result = "";
-        for(int i=0; i<words.size(); i++){
-            result += words[i];
-
-            if(i !=words.size()-1){
-                result += " ";
-            }
-        }
-
-        return result;
-    }
-};
-
-
-
-
 // class Solution {
 // public:
 //     string reverseWords(string s) {
-//         // 1. Remove extra spaces
-//         int n = s.size();
-//         int i = 0, j = 0;
+//         stringstream ss(s);
+//         string word;
+//         vector<string> words;
 
-//         while (j < n) {
-//             // skip leading spaces
-//             while (j < n && s[j] == ' ') j++;
-//             if (j >= n) break;
-
-//             // add space before next word (not before first)
-//             if (i > 0) s[i++] = ' ';
-
-//             // copy word
-//             int start = j;
-//             while (j < n && s[j] != ' ') s[i++] = s[j++];
-
-//             // reverse the word in-place
-//             reverse(s.begin() + start, s.begin() + i);
+//         while(ss >> word){
+//             words.push_back(word);
 //         }
 
-//         // remove trailing
-//         s.resize(i);
+//         reverse(words.begin(), words.end());
 
-//         // 2. Reverse the entire string
-//         reverse(s.begin(), s.end());
+//         string result = "";
+//         for(int i=0; i<words.size(); i++){
+//             result += words[i];
 
-//         return s;
+//             if(i !=words.size()-1){
+//                 result += " ";
+//             }
+//         }
+
+//         return result;
 //     }
 // };
+
+
+
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n = s.size();
+
+        // Step 1: Remove extra spaces (in-place)
+        int i = 0, j = 0;
+
+        while (j < n) {
+            // Skip leading spaces
+            while (j < n && s[j] == ' ') j++;
+            if (j >= n) break;
+
+            // Add a single space before next word (except before first)
+            if (i > 0) s[i++] = ' ';
+
+            // Copy the next word
+            int start = i;
+            while (j < n && s[j] != ' ') {
+                s[i++] = s[j++];
+            }
+
+            // Reverse the current word in place
+            reverse(s.begin() + start, s.begin() + i);
+        }
+
+        // Resize to remove trailing spaces
+        s.resize(i);
+
+        // Step 2: Reverse the entire cleaned string
+        reverse(s.begin(), s.end());
+
+        return s;
+    }
+};
+
