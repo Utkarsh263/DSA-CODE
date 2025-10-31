6d3053f44e8658b5a12cb18dd4eb5b69469ca045
Utkarsh <utkarshkohli821@gmail.com>
2025-11-01 00:29:34 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (68.84%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,21 +1,43 @@
+// class Solution {
+// public:
+//     string longestCommonPrefix(vector<string>& strs) {
+//         if(strs.empty()){
+//             return "";
+//         }
+
+//         for(int i=0; i<strs[0].size(); i++){
+//             char ch = strs[0][i];
+
+//             for(int j=1; j<strs.size(); j++){
+
+//                 if(i >= strs[j].size() || strs[j][i] != ch){
+//                     return strs[0].substr(0,i);
+//                 }
+//             }
+//         }
+
+//         return strs[0];
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        if(strs.empty()){
-            return "";
-        }
+        if (strs.empty()) return "";
 
-        for(int i=0; i<strs[0].size(); i++){
-            char ch = strs[0][i];
+        string prefix = strs[0]; // take first string as the initial prefix
 
-            for(int j=1; j<strs.size(); j++){
-
-                if(i >= strs[j].size() || strs[j][i] != ch){
-                    return strs[0].substr(0,i);
-                }
+        for (int i = 1; i < strs.size(); i++) {
+            // while current string doesn't start with prefix
+            while (strs[i].find(prefix) != 0) {
+                prefix = prefix.substr(0, prefix.size() - 1); // cut last char
+                if (prefix.empty()) return ""; // no common prefix
             }
         }
 
-        return strs[0];
+        return prefix;
     }
 };
