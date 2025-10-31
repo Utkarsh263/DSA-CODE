a77d531e7eb04e17aab94a53b61798b308ef0248
Utkarsh <utkarshkohli821@gmail.com>
2025-10-31 12:32:53 +0530

    Time: 3 ms (89.72%) | Memory: 23.8 MB (81.05%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& arr) {
-        int n = arr.size();
-        sort(arr.begin(), arr.end());
-        vector<vector<int>> ans;
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>>result;
 
-        for(int i=0; i<n; i++){
-            int start= arr[i][0];
-            int end = arr[i][1];
-
-            if(!ans.empty() && end <= ans.back()[1]){
-                continue;  // Already merged interval
-            }
-
-            for(int j=i+1; j<n; j++){
-                if(arr[j][0] <= end){
-                    end = max(end , arr[j][1]);
-                }else{
-                    break;
-                }
-            }
-
-            ans.push_back(vector<int>{start, end});
+        if(intervals.empty()){
+            return result;
         }
-        return ans;
+
+        sort(intervals.begin(), intervals.end());
+
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+
+        for(int i=1; i<intervals.size(); i++){
+            int currstart = intervals[i][0];
+            int currend = intervals[i][1];
+
+            //overlapping case
+            if(currstart <= end){
+                end = max(end , currend);
+            }else{
+                result.push_back({start, end});
+                start = currstart;
+                end = currend;
+            }
+        }
+
+        // push the last interval
+        result.push_back({start, end});
+
+        return result;
     }
 };
