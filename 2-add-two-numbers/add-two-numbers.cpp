2523cd2c869d48d60cec0e99e2fbb288c4c1fa2d
Utkarsh <utkarshkohli821@gmail.com>
2025-11-02 18:56:02 +0530

    Time: 3 ms (34.61%) | Memory: 77.2 MB (13.76%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -1,45 +1,29 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode(-1);
-        ListNode* curr = dummy;
-        ListNode* temp1 = l1;
-        ListNode* temp2 = l2;
-        int carry =0;
-        while(temp1 != NULL || temp2 != NULL){
-            int sum = carry;
-            if(temp1){
-                sum = sum+ temp1->val;
+        ListNode* dummy = new ListNode(0);
+        ListNode* temp = dummy;
+        int carry = 0;
+
+        while (l1 != NULL || l2 != NULL || carry != 0) {
+            int sum = 0;
+
+            if (l1 != NULL) {
+                sum += l1->val;
+                l1 = l1->next;
             }
-            if(temp2){
-                sum = sum + temp2->val;            
+
+            if (l2 != NULL) {
+                sum += l2->val;
+                l2 = l2->next;
             }
-            ListNode* newNode = new ListNode(sum %10);
+
+            sum += carry;
             carry = sum / 10;
 
-            curr->next = newNode;
-            curr= curr->next;
-
-            if(temp1){
-                temp1 = temp1->next;
-            }
-            if(temp2){
-                temp2= temp2->next;
-            }
-        }
-        if(carry){
-            ListNode* newNode= new ListNode(carry);
-            curr->next = newNode;
+            ListNode* node = new ListNode(sum % 10);
+            temp->next = node;
+            temp = temp->next;
         }
 
         return dummy->next;
