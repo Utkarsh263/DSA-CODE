dcfb24ea3c98bf74c0aff9a76e94329cc569e472
Utkarsh <utkarshkohli821@gmail.com>
2025-11-03 19:20:01 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (70.01%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
-    const long long MOD = 1e9 + 7;
+    long long mod = 1e9 + 7;
 
-    long long modPow(long long base , long long exp){
+    long long power(long long base, long long exp) {
         long long result = 1;
-        base = base % MOD;
-
-        while(exp >0){
-            if(exp %2 == 1){
-                result = (result * base)%MOD;
-                exp--;
-            }else{
-                base = (base* base) % MOD;
-                exp = exp/2;
+        while (exp > 0) {
+            if (exp % 2 == 1) {
+                result = (result * base) % mod;
             }
+            base = (base * base) % mod;
+            exp /= 2;
         }
-
         return result;
     }
+
     int countGoodNumbers(long long n) {
-        long long evencount = (n+1)/2;
-        long long oddcount = n/2;
+        long long evenCount = (n + 1) / 2; // ceil(n/2)
+        long long oddCount = n / 2;        // floor(n/2)
 
-        long long part1 = modPow(5, evencount);
-        long long part2 = modPow(4, oddcount);
+        long long evenWays = power(5, evenCount);
+        long long oddWays = power(4, oddCount);
 
-        return (part1*part2)%MOD;
-    
+        return (evenWays * oddWays) % mod;
     }
 };
