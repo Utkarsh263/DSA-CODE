36949e9a6b561dc35fe389892d5eb666abdde1b9
Utkarsh <utkarshkohli821@gmail.com>
2025-11-03 22:16:48 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (28.83%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    void backtrack(vector<int> &nums, int i, vector<int> &subset , vector<vector<int>> &result){
-        if(i== nums.size()){
+    void backtrack(vector<int> &nums, int i ,vector<int> &subset , vector<vector<int>>&result){
+
+        if(i == nums.size()){
             result.push_back(subset);
             return;
         }
 
-        // exclusion case
-        backtrack(nums, i+1, subset, result);
-        // inclusion case
+        // Exclusion
+        backtrack(nums, i+1 , subset , result);
+
+        // Inclusion
         subset.push_back(nums[i]);
-        backtrack(nums, i+1, subset, result);
-        subset.pop_back(); // Jacket example to put off older one to try newer one 
+        backtrack(nums, i+1, subset , result);
+        subset.pop_back(); //backtrack
+
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>>result;
-        vector<int>subset;
-        backtrack(nums,0,subset, result);
+        vector<int> subset;
+        backtrack(nums, 0, subset, result);
         return result;
     }
 };
