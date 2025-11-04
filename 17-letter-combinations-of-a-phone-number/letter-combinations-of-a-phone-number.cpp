8948fcaf947fc411a4ff8c862effc936913ae722
Utkarsh <utkarshkohli821@gmail.com>
2025-11-04 12:26:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (87.11%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    void backtrack(int ind, string &path ,string &digits,vector<string> &ans, unordered_map<char, string> &mp){
-        if(ind == digits.size()){
+    void backtrack(int index, string &path, string &digits, vector<string>&ans , unordered_map<char, string>&mp){
+        if(index == digits.size()){
             ans.push_back(path);
             return;
         }
 
-        string letters = mp[digits[ind]];
+        string letters = mp[digits[index]];
         for(char ch : letters){
-            path.push_back(ch);
-            backtrack(ind+1 , path, digits, ans, mp);
-            path.pop_back();
+            path.push_back(ch);  // recursive add to the path
+            backtrack(index+1, path, digits, ans, mp); // recursive call
+            path.pop_back();  // backtrack the path
         }
-
     }
     vector<string> letterCombinations(string digits) {
         vector<string> ans;
@@ -20,15 +19,16 @@ public:
             return ans;
         }
 
-        // hash table 
-        unordered_map<char, string> mp ={
-            {'2',"abc"}, {'3',"def"}, {'4',"ghi"},
-            {'5',"jkl"},{'6',"mno"},{'7',"pqrs"},
-            {'8',"tuv"},{'9',"wxyz"}
+        // mapping of digits to letters
+        unordered_map<char, string>mp= {
+             {'2', "abc"}, {'3', "def"}, {'4', "ghi"},
+            {'5', "jkl"}, {'6', "mno"}, {'7', "pqrs"},
+            {'8', "tuv"}, {'9', "wxyz"}
         };
 
-        string path="";
-        backtrack(0,path, digits, ans, mp);
+        string path = "";
+        backtrack(0, path, digits, ans, mp);
         return ans;
+
     }
 };
