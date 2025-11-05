32313cdbf19d5cf0b441723cecdfe6a11bd70c17
Utkarsh <utkarshkohli821@gmail.com>
2025-11-05 15:20:41 +0530

    Time: 846 ms (18.28%) | Memory: 11.5 MB (51.11%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,40 +1,37 @@
 class Solution {
 public:
-int rows, cols;
-    bool dfs(vector<vector<char>> &board , int r , int c, string &word, int idx){
-        if(idx== word.size()){
+    bool backtrack(vector<vector<char>>& board, string word, int i, int j , int index){
+        if(index == word.size()){
             return true;
         }
 
-        if(r<0 || r>= rows || c<0 || c>= cols){
-            return false;
-        }
-        if(board[r][c] != word[idx]){
+        if (i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || board[i][j] != word[index]){
             return false;
         }
 
-        char saved = board[r][c];
-        board[r][c] = '#'; // replace character with symbol
+        char temp = board[i][j];
+        board[i][j] = '#';
 
-        bool found = dfs(board, r + 1, c, word, idx + 1) ||
-                     dfs(board, r - 1, c, word, idx + 1) ||
-                     dfs(board, r, c + 1, word, idx + 1) ||
-                     dfs(board, r, c - 1, word, idx + 1);
+        bool found = backtrack(board, word,i-1, j, index+1)||
+                    backtrack(board, word,i+1, j, index+1)||
+                    backtrack(board, word,i, j-1, index+1)||
+                    backtrack(board, word,i, j+1, index+1);
+        board[i][j] = temp; //undo the visit and backtrack
 
-        board[r][c] = saved; // restore the character
         return found;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        rows = board.size();
-        cols = board[0].size();
+        int rows = board.size();
+        int cols = board[0].size();
 
-        for(int r=0; r<rows; r++){
-            for(int c=0; c<cols; c++){
-                if(dfs(board, r, c, word , 0)){
+        for(int i=0; i< rows; i++){
+            for(int j = 0; j< cols; j++){
+                if(backtrack(board, word, i, j, 0)){
                     return true;
                 }
             }
         }
+
         return false;
     }
 };
