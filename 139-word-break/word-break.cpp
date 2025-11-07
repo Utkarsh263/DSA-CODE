d6874e94c549edb47c6a1caccfca936d14eab7c7
Utkarsh <utkarshkohli821@gmail.com>
2025-11-07 12:05:56 +0530

    Time: 16 ms (22.09%) | Memory: 17.1 MB (27.62%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,18 +1,55 @@
+// class Solution {
+// public:
+//     bool backtrack(int start, string &s, unordered_set<string>&dict){
+//         if(start == s.size()){
+//             return true;
+//         }
+
+//         for(int end = start+1; end <= s.size(); end++){
+//             string word = s.substr(start, end-start);
+//             if(dict.count(word)){
+//                 if(backtrack(end, s, dict)){
+//                     return true;
+//                 }
+//             }
+//         }
+
+//         return false;
+//     }
+//     bool wordBreak(string s, vector<string>& wordDict) {
+//         unordered_set<string> dict(wordDict.begin(), wordDict.end());
+//         return backtrack(0, s, dict);
+//     }
+// };
+
+
+
+
+
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        vector<bool> dp(s.size() + 1, false);
-        dp[0] = true;
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        vector<int> memo(s.size(), -1);  // -1 = not visited
+        
+        return canBreak(0, s, dict, memo);
+    }
 
-        for (int i = 1; i <= s.size(); i++) {
-            for (const string& w : wordDict) {
-                int start = i - w.length();
-                if (start >= 0 && dp[start] && s.substr(start, w.length()) == w) {
-                    dp[i] = true;
-                    break;
-                }
+private:
+    bool canBreak(int start, string &s, unordered_set<string> &dict, vector<int> &memo) {
+        // Base case: reached end successfully
+        if (start == s.size()) return true;
+        
+        // If already computed
+        if (memo[start] != -1) return memo[start];
+        
+        // Try all possible splits
+        for (int end = start + 1; end <= s.size(); ++end) {
+            string word = s.substr(start, end - start);
+            if (dict.count(word) && canBreak(end, s, dict, memo)) {
+                return memo[start] = 1;  // store result & return true
             }
         }
-        return dp[s.size()];        
+        return memo[start] = 0;  // store result & return false
     }
 };
