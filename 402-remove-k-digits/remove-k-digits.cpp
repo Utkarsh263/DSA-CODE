b8805935f6a9cc7c15ce6a94127032c7ca860c60
Utkarsh <utkarshkohli821@gmail.com>
2025-11-09 23:18:07 +0530

    Time: 3 ms (63.22%) | Memory: 11.3 MB (75.55%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -4,26 +4,24 @@ public:
         string st = "";
 
         for(char digit : num){
-            // while last digit > current and we can still remove
-            while(!st.empty() && k > 0 && st.back() > digit){
+            while(!st.empty() && k>0 && st.back() > digit){
                 st.pop_back();
                 k--;
             }
             st.push_back(digit);
         }
-        // if still digits are left to be trimmed
-        while(k > 0 && !st.empty()){
+
+        while(k>0 && !st.empty()){
             st.pop_back();
             k--;
         }
 
-        int i=0;
+        int i=0; 
         while(i< st.size() && st[i]=='0'){
             i++;
         }
 
-        string result= st.substr(i);
+        string result = st.substr(i);
         return result.empty() ? "0" : result;
-
     }
 };
