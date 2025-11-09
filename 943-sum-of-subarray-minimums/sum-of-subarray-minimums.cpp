f3b9de79a12276a20e7a660deaf6b8f4f9e4d7e5
Utkarsh <utkarshkohli821@gmail.com>
2025-11-09 17:18:47 +0530

    Time: 35 ms (7.92%) | Memory: 48.3 MB (16.63%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -2,36 +2,37 @@ class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
         int n = arr.size();
-        vector<int> left(n), right(n);
-        stack<pair<int,int>> st1, st2;
-        const int MOD = 1e9 + 7;
+        vector<int>left(n) , right(n);
+        stack<pair<int, int>> st1, st2;
+        const int MOD = 1e9+7;
 
-        // Step 1: Previous Less Element (PLE)
-        for (int i = 0; i < n; i++) {
-            int count = 1;
-            while (!st1.empty() && st1.top().first > arr[i]) {
+        // Previous greater element
+        for(int i=0; i<n; i++){
+            int count=1;
+            while(!st1.empty() && st1.top().first> arr[i]){
                 count += st1.top().second;
                 st1.pop();
             }
+
             st1.push({arr[i], count});
-            left[i] = count;
+            left[i]= count;
         }
 
-        // Step 2: Next Less or Equal Element (NLE)
-        for (int i = n - 1; i >= 0; i--) {
-            int count = 1;
-            while (!st2.empty() && st2.top().first >= arr[i]) {
+        //Next less or equal element
+        for(int i=n-1; i>=0; i--){
+            int count=1;
+            while(!st2.empty() && st2.top().first>= arr[i]){
                 count += st2.top().second;
                 st2.pop();
             }
+
             st2.push({arr[i], count});
             right[i] = count;
         }
 
-        // Step 3: Contribution of each element
-        long long result = 0;
-        for (int i = 0; i < n; i++) {
-            result = (result + (long long)arr[i] * left[i] * right[i]) % MOD;
+        long long result =0;
+        for(int i=0; i<n; i++){
+            result = (result + (long long)arr[i]*left[i]*right[i]) % MOD;
         }
 
         return result;
