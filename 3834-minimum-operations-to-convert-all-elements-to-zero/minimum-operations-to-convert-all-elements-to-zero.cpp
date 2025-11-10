c1fc584c28e8a342dca65da138a5290efdab24b0
Utkarsh <utkarshkohli821@gmail.com>
2025-11-10 11:27:24 +0530

    Time: 19 ms (85.39%) | Memory: 222.1 MB (69.10%) - LeetSync

--- 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.cpp
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.cpp
@@ -3,23 +3,18 @@ public:
     int minOperations(vector<int>& nums) {
         int ans=0;
         stack<int> st;
-        st.push(0); // senitel value zero
+        st.push(0);
 
-        for(int num : nums){
-            // Pop all values strictly greater than current num
+        for(int num: nums){
+
             while(!st.empty() && st.top() > num){
                 st.pop();
             }
-            // If stack now empty OR the top value < current num  
-            // => we need a new operation
 
-            if(st.empty() || st.top()< num){
+            if(st.empty() || st.top() < num){
                 ans++;
                 st.push(num);
             }
-
-            // If st.top() == num, we do nothing (we can reuse an existing operation)
-
         }
 
         return ans;
