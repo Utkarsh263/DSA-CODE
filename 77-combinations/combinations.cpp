87c20d2403a8f3c587b2daf5161ba366939a46e8
Utkarsh <utkarshkohli821@gmail.com>
2026-01-08 20:18:38 +0530

    Time: 71 ms (42.85%) | Memory: 82.6 MB (31.85%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -3,21 +3,21 @@ public:
     vector<vector<int>>result;
     vector<int>path;
 
-    void backtrack(int start , int n , int k){
+    void backtrack(int start, int n , int k){
 
         if(path.size()==k){
             result.push_back(path);
             return;
         }
 
-        for(int i=start; i<=n; i++){
-            path.push_back(i);
-            backtrack(i+1, n, k);
-            path.pop_back();  // backtrack the paths 
+        for(int i= start; i<=n; i++){
+            path.push_back(i);  // make the choice 
+            backtrack(i+1 , n, k);
+            path.pop_back();  // unchoose the choice
         }
     }
     vector<vector<int>> combine(int n, int k) {
-        backtrack(1 , n, k);
+        backtrack(1, n, k);
         return result;
     }
 };
