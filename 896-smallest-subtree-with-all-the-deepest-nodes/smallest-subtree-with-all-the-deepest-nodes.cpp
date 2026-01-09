f25e4e244c45d383fe374e6cdb7a85b469344299
Utkarsh <utkarshkohli821@gmail.com>
2026-01-09 14:28:06 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (52.91%) - LeetSync

--- 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -11,24 +11,25 @@
  */
 class Solution {
 public:
-    pair<TreeNode* , int>dfs(TreeNode* root){
+    pair<TreeNode* , int> dfs(TreeNode* root){
         if(root == NULL){
-            return {NULL , 0};
-
+            return {NULL , 0}; 
         }
 
-        pair<TreeNode* , int>left = dfs(root->left);
-        pair<TreeNode*, int>right = dfs(root->right);
+        pair<TreeNode* , int> left = dfs(root->left);
+        pair<TreeNode*, int> right = dfs(root->right);
 
+        // Comparing depth 
         if(left.second > right.second){
             return {left.first, left.second+1};
         }else if(right.second > left.second){
-            return {right.first, right.second+1};
+            return {right.first , right.second+1};
         }
 
-        return {root, left.second+1};
+        return {root , left.second+1};
     }
     TreeNode* subtreeWithAllDeepest(TreeNode* root) {
         return dfs(root).first;
+        
     }
 };
