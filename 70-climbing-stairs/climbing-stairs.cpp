18ab2bc71964477a1189a2c86ee3f248f825d1e1
Utkarsh <utkarshkohli821@gmail.com>
2026-01-10 17:22:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (33.62%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        // To climb stairs n , we can find n-1 for 1 and n-2 as 2 stairs at a time 
-
         vector<int>dp(n+1, 0);
 
-        dp[0] = 1; // its a way 
+        dp[0] = 1;
         dp[1] = 1;
 
         for(int i=2; i<=n; i++){
