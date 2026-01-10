a5b87483c4a364ffc6ab4af791979381cb5a7bf0
Utkarsh <utkarshkohli821@gmail.com>
2026-01-10 16:53:18 +0530

    Time: 17 ms (51.50%) | Memory: 18.2 MB (67.29%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -4,19 +4,21 @@ public:
         int n = s1.size();
         int m = s2.size();
 
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
 
-        for(int i=1; i<=n; i++){  // s2 is empty
-            dp[i][0] = dp[i-1][0] + s1[i-1];
+        // when s2 is empty 
+        for(int i=1; i<=n; i++){
+            dp[i][0] = dp[i-1][0] + s1[i-1]; 
         }
 
-        for(int j=1; j<=m; j++){ // s1 is empty
+        // when s1 is empty 
+        for(int j=1; j<= m; j++){
             dp[0][j] = dp[0][j-1] + s2[j-1];
         }
 
         for(int i=1; i<=n; i++){
             for(int j=1; j<=m; j++){
-                if(s1[i-1]==s2[j-1]){
+                if(s1[i-1]== s2[j-1]){
                     dp[i][j] = dp[i-1][j-1];
                 }else{
                     dp[i][j] = min(
