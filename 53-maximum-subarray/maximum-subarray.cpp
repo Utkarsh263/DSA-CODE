1e0ea4d398005f6aff3be4b3ef4d9ef3d7a5edac
Utkarsh <utkarshkohli821@gmail.com>
2026-01-19 15:17:10 +0530

    Time: 0 ms (100.00%) | Memory: 71.6 MB (97.56%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        // We have to apply Kadane Algorithm to find maximum Subarray Sum 
+        // Kadane Algorithm 
 
-        int currSum= 0;
+        int n = nums.size();
+        int currSum =0;
         int maxSum = INT_MIN;
-        
 
-        for(int x : nums){
-            currSum += x;
-            maxSum = max(currSum, maxSum);
-            if(currSum < 0){
-                currSum = 0;
+        for(int i=0; i<n; i++){
+            currSum += nums[i];
+            maxSum = max(maxSum , currSum);
+
+            if(currSum <0){
+                currSum =0;
             }
         }
 
