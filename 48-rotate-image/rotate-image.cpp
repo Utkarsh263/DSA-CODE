88c372855e0eeb3bc70bcaebd2c066dca34d8ffd
Utkarsh <utkarshkohli821@gmail.com>
2026-01-19 15:34:51 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (35.63%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,40 +1,19 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        // Transpose the matrix 
-        int n = matrix.size();
-        for(int i=0; i<n; i++){
-            for(int j = i+1; j<n; j++){
-                swap(matrix[i][j] , matrix[j][i]);
+        // Transpose the matrix and reverse the rows 
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        for(int i=0; i<m;i++){
+            for(int j=i; j<n; j++){
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
 
-        // Reverse the rows 
-        for(int i=0; i<n; i++){
+        // Reverse each row 
+        for(int i=0; i<m;i++){
             reverse(matrix[i].begin(), matrix[i].end());
         }
     }
 };
-
-// Brute Force approach 
-// class Solution {
-// public:
-//     void rotate(vector<vector<int>>& matrix) {
-//         int n = matrix.size();
-//         vector<vector<int>> temp(n, vector<int>(n));
-
-//         // Place elements in rotated positions
-//         for(int i = 0; i < n; i++){
-//             for(int j = 0; j < n; j++){
-//                 temp[j][n - 1 - i] = matrix[i][j];
-//             }
-//         }
-
-//         // Copy back to original matrix
-//         for(int i = 0; i < n; i++){
-//             for(int j = 0; j < n; j++){
-//                 matrix[i][j] = temp[i][j];
-//             }
-//         }
-//     }
-// };
