3bfb64714b995977fd45fa19300353b499acebae
Utkarsh <utkarshkohli821@gmail.com>
2026-01-20 00:43:38 +0530

    Time: 51 ms (47.42%) | Memory: 29.1 MB (70.50%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,78 +1,44 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>>res;
-        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
         int n = nums.size();
 
-        for(int i=0; i<nums.size(); i++){
-            if(i > 0 && nums[i]==nums[i-1]){
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n; i++) {
+
+            // Skip duplicate fixed elements
+            if (i > 0 && nums[i] == nums[i - 1]) 
                 continue;
-            }
 
-            int left = i+1;
-            int right = n-1;
+            int left = i + 1;
+            int right = n - 1;
 
-            while(left < right){
-                int sum = nums[i]+nums[left]+nums[right];
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
 
-                if(sum==0){
-                    res.push_back({nums[i], nums[left], nums[right]});
-                    while(left < right && nums[left]== nums[left+1]){
-                    left++;
-                    }
-                    while(left < right && nums[right]==nums[right-1]){
-                    right--;
-                    }
-
+                if (sum == 0) {
+                    ans.push_back({nums[i], nums[left], nums[right]});
                     left++;
                     right--;
-                }else if (sum < 0){
+
+                    // Skip duplicate left elements
+                    while (left < right && nums[left] == nums[left - 1])
+                        left++;
+
+                    // Skip duplicate right elements
+                    while (left < right && nums[right] == nums[right + 1])
+                        right--;
+                }
+                else if (sum < 0) {
                     left++;
-                }else{
+                }
+                else {
                     right--;
                 }
-                
             }
         }
-        return res;
+        return ans;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Solution {
-// public:
-//     vector<vector<int>> threeSum(vector<int>& nums) {
-//         int n = nums.size();
-//         set<vector<int>>st;
-//         for(int i=0; i<n; i++){
-//             set<int> hashset;
-//             for(int j=i+1; j<n; j++){
-//                 int third = -(nums[i]+nums[j]);
-//                 if(hashset.find(third) !=hashset.end()){
-//                     vector<int> temp = {nums[i], nums[j], third};
-//                     sort(temp.begin(), temp.end());
-//                     st.insert(temp);
-//                 }
-//                 hashset.insert(nums[j]);
-//             }
-//         }
-
-//         vector<vector<int>> ans(st.begin(), st.end());
-
-//         return ans;
-
-//     }
-// };
