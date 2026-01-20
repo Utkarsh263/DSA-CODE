3d963d705c5ed6c8192025ac722654a9957553a5
Utkarsh <utkarshkohli821@gmail.com>
2026-01-20 21:50:54 +0530

    Time: 33 ms (91.32%) | Memory: 45.5 MB (40.89%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map <int, int> mp;
-        mp[0]=1;
+        unordered_map<int, int>mp;  //prefixSum -> Freq
+        mp[0] = 1; // Base Case
 
-        int count=0;
         int prefixSum =0;
+        int count = 0;
 
         for(int num : nums){
+
             prefixSum += num;
 
-            if(mp.find(prefixSum -k) != mp.end()){
+            if(mp.find(prefixSum-k) != mp.end()){
                 count += mp[prefixSum-k];
             }
 
