dd483a72450f471f7d666e8900beec1e9311c1b6
Utkarsh <utkarshkohli821@gmail.com>
2026-01-22 18:45:17 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (48.05%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
@@ -2,25 +2,23 @@ class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
         int totalSum =0;
-        int currMax =0 , maxSum = nums[0];
-        int currMin =0, minSum = nums[0];
+        int currMax =0, maxSum = nums[0];
+        int currMin = 0, minSum = nums[0];
 
         for(int x : nums){
             totalSum += x;
 
-            // Kadane for MaxSum 
-            currMax = max(x,currMax+x);
+            currMax = max(x , currMax+x);
             maxSum = max(maxSum , currMax);
 
-            // Kadane for MinSum 
-            currMin = min(x , currMin+x);
-            minSum = min(minSum, currMin);
+            currMin = min(x , currMin +x);
+            minSum = min(minSum , currMin);
         }
 
-        if(maxSum <0){
+        if(maxSum < 0){
             return maxSum;
         }
 
-        return max(maxSum , totalSum-minSum);
+        return max(maxSum , totalSum - minSum);
     }
 };
