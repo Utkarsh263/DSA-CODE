7788531692aef4cfc1006f49490c245f71e90580
Utkarsh <utkarshkohli821@gmail.com>
2026-01-22 17:56:18 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (58.43%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int currMax= nums[0];
-        int currMin = nums[0];
+        // Here we have to track 2 numbers to track maximum and minimum  number 
+        int maxEnd = nums[0];
+        int minEnd = nums[0];
         int ans = nums[0];
 
         for(int i=1; i<nums.size(); i++){
-            if(nums[i]<0){
-                swap(currMax , currMin);
-            }
+            int curr = nums[i];
 
-            currMax = max(nums[i] , nums[i]*currMax);
-            currMin = min(nums[i] , nums[i]*currMin);
+            int tempMax = max({curr, maxEnd*curr , curr*minEnd});
+            int tempMin = min({curr, maxEnd*curr , minEnd*curr});
 
-            ans = max(ans , currMax);
+            maxEnd = tempMax;
+            minEnd = tempMin;
+
+            ans = max(ans, maxEnd);
         }
 
         return ans;
