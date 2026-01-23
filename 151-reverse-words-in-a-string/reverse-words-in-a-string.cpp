e8a3d7b397f9ada95a465ad5f819eff2852c8e00
Utkarsh <utkarshkohli821@gmail.com>
2026-01-23 15:53:13 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (32.97%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,65 +1,23 @@
-// class Solution {
-// public:
-//     string reverseWords(string s) {
-//         stringstream ss(s);
-//         string word;
-//         vector<string> words;
-
-//         while(ss >> word){
-//             words.push_back(word);
-//         }
-
-//         reverse(words.begin(), words.end());
-
-//         string result = "";
-//         for(int i=0; i<words.size(); i++){
-//             result += words[i];
-
-//             if(i !=words.size()-1){
-//                 result += " ";
-//             }
-//         }
-
-//         return result;
-//     }
-// };
-
-
-
-
 class Solution {
 public:
     string reverseWords(string s) {
-        int n = s.size();
+        string word;
+        stringstream ss(s);
+        vector<string>words;
 
-        // Step 1: Remove extra spaces (in-place)
-        int i = 0, j = 0;
-
-        while (j < n) {
-            // Skip leading spaces
-            while (j < n && s[j] == ' ') j++;
-            if (j >= n) break;
-
-            // Add a single space before next word (except before first)
-            if (i > 0) s[i++] = ' ';
-
-            // Copy the next word
-            int start = i;
-            while (j < n && s[j] != ' ') {
-                s[i++] = s[j++];
-            }
-
-            // Reverse the current word in place
-            reverse(s.begin() + start, s.begin() + i);
+        while(ss >> word){
+            words.push_back(word);
         }
 
-        // Resize to remove trailing spaces
-        s.resize(i);
+        string result;
 
-        // Step 2: Reverse the entire cleaned string
-        reverse(s.begin(), s.end());
+        for(int i = words.size()-1; i>= 0; i--){
+            result += words[i];
+            if(i > 0){
+                result += " ";
+            }
+        }
 
-        return s;
+        return result;
     }
 };
-
