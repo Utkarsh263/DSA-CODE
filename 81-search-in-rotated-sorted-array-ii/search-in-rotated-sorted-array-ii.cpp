f381e57872f715b9125e85f8309b4b4aa073c6cd
Utkarsh <utkarshkohli821@gmail.com>
2026-01-25 15:39:13 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (20.63%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -5,17 +5,16 @@ public:
         int right = nums.size()-1;
 
         while(left <= right){
+
             int mid = (left+right)/2;
 
             if(nums[mid]==target){
                 return true;
             }
 
-            // duplicates case
             if(nums[left]==nums[mid] && nums[mid]==nums[right]){
                 left++;
                 right--;
-                continue;
             }else if(nums[left] <= nums[mid]){
                 if(nums[left] <= target && target < nums[mid]){
                     right = mid-1;
@@ -23,13 +22,14 @@ public:
                     left = mid+1;
                 }
             }else{
-                 if(nums[mid] < target && target <= nums[right]){
+                if(nums[mid] < target && target <= nums[right]){
                     left = mid+1;
                 }else{
-                    right = mid-1;
+                    right =mid-1;
                 }
             }
         }
+
         return false;
     }
 };
