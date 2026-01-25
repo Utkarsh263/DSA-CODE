4418ca6bf6cae1627f998c3c903bd13312f6c9ad
Utkarsh <utkarshkohli821@gmail.com>
2026-01-25 17:24:51 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (33.54%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -3,15 +3,16 @@ public:
     int singleNonDuplicate(vector<int>& nums) {
         int left =0;
         int right = nums.size()-1;
+
         while(left < right){
             int mid = (left + right)/2;
 
-            // checking for the pairs 
-            if(mid %2 ==1){// first elemt of pair so mid--
+            // If mid is odd , make it even to make pair intact 
+            if(mid %2 == 1){
                 mid--;
             }
 
-            if(nums[mid] == nums[mid+1]){
+            if(nums[mid]==nums[mid+1]){
                 left = mid+2;
             }else{
                 right = mid;
@@ -21,45 +22,3 @@ public:
         return nums[left];
     }
 };
-
-
-
-
-
-
-
-// class Solution {
-// public:
-//     int singleNonDuplicate(vector<int>& arr) {
-//         int n = arr.size();
-//         if(n==1){
-//             return  arr[0];
-//         }
-//         if(arr[0] != arr[1]){
-//             return arr[0];    
-//         }
-//         if(arr[n-1] != arr[n-2]){
-//             return arr[n-1];
-//         }
-
-//         int low =1;
-//         int high = n-2;
-
-//         while(low <= high){
-//             int mid = (low + high)/2;
-
-//             if(arr[mid] != arr[mid+1] && arr[mid] != arr[mid-1]){
-//                 return arr[mid];
-//             }
-
-//             if((mid % 2 == 1 && arr[mid] == arr[mid-1]) || (mid %2 ==0 && arr[mid] == arr[mid+1])){
-//                 low = mid+1;
-//             }else{
-//                 high = mid-1;
-//             }
-//         }
-
-//         return -1;
-
-//     }
-// };
