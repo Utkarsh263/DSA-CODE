ccdb0d4d36339d43b7cd05fca36319c1aca76b54
Utkarsh <utkarshkohli821@gmail.com>
2026-01-25 17:32:53 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (92.11%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -5,20 +5,19 @@ public:
         int right = nums.size()-1;
 
         while(left < right){
-            int mid = (left + right)/2;
+            int mid = (left+right)/2;
 
-            // If mid is odd , make it even to make pair intact 
-            if(mid %2 == 1){
+            if(mid % 2 == 1){
                 mid--;
             }
 
-            if(nums[mid]==nums[mid+1]){
+            if(nums[mid]== nums[mid+1]){
                 left = mid+2;
             }else{
                 right = mid;
             }
         }
 
-        return nums[left];
+        return nums[right];
     }
 };
