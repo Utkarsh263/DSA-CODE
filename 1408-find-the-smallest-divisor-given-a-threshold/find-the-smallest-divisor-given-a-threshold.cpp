b8eb1143f8d10dc2e48e055ee8131ab4bae74859
Utkarsh <utkarshkohli821@gmail.com>
2026-01-26 17:42:40 +0530

    Time: 8 ms (55.48%) | Memory: 26.2 MB (14.82%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,71 +1,32 @@
 class Solution {
 public:
-    int sumAfterDivision(vector<int> &nums, int divisor){
-        int total=0;
-        for(int n : nums){
-            total += (n+divisor-1)/divisor;
-        }
-        return total;
-    }
-    int smallestDivisor(vector<int>& nums, int threshold) {
-        int low =1;
-        int high = *max_element(nums.begin(), nums.end());
-
-        while(low < high){
-            int mid = (low + high)/2;
-            if(sumAfterDivision(nums, mid) > threshold){
-                low = mid+1;
-            }else{
-                high = mid;
+    bool isValid(vector<int>&nums, int threshold , int divisor){
+        long long sum = 0;
+        for(int x : nums){
+            sum += (x+divisor-1)/divisor;
+            if(sum > threshold){
+                return false;
             }
         }
 
-        return low;
+        return true;
+    }
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int low = 1;
+        int high = *max_element(nums.begin(), nums.end());
+        int ans = high;
+
+        while(low <= high){
+            int mid = (low + high)/2;
+
+            if(isValid(nums, threshold, mid)){
+                ans = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+
+        return ans;
     }
 };
-
-
-
-
-
-
-// #include <bits/stdc++.h>
-// using namespace std;
-
-// class Solution {
-// public:
-//     int maximum (vector<int> &nums){
-//         int maxi = INT_MIN;
-//         for(int i=0; i<nums.size(); i++){
-//             maxi = max(nums[i], maxi);
-//         }
-
-//         return maxi;
-//     }
-
-//     int sumbyD(vector<int> &nums, int d){
-//         int n = nums.size();
-//         int sum = 0;
-//         for(int i=0; i<n ; i++){
-//             sum += ceil((double)nums[i]/(double)d);
-//         }
-//         return sum;
-//     }
-
-//     int smallestDivisor(vector<int>& nums, int threshold) {
-//         int low =1;
-//         int high = maximum(nums);
-//         while(low <= high){
-//             int mid = (low+high)/2;
-
-//             if(sumbyD(nums,mid) <= threshold){
-//                 high = mid-1;
-//             }else{
-//                 low = mid+1;
-//             }
-//         }
-
-//         return low;
-
-//     }
-// };
