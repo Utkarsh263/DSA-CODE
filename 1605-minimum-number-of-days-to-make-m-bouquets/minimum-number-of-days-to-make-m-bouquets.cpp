c1be993d2f2e3dd1681a2246043dff6059ec8b00
Utkarsh <utkarshkohli821@gmail.com>
2026-01-26 17:20:30 +0530

    Time: 23 ms (55.03%) | Memory: 70.5 MB (45.47%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     bool canMake(vector<int>&bloomDay , int m , int k , int day){
-        int bouquets = 0;
         int flowers =0;
+        int bouquets = 0;
 
         for(int b : bloomDay){
-            if(b <=day){
+            if(b <= day){
                 flowers++;
-                if(flowers == k){
+                if(flowers==k){
                     bouquets++;
-                    flowers = 0;
+                    flowers= 0;
                 }
             }else{
-                flowers = 0;
+                flowers = 0; // Break the adjancency rule 
             }
         }
 
@@ -20,24 +20,24 @@ public:
     }
     int minDays(vector<int>& bloomDay, int m, int k) {
         long long n = bloomDay.size();
+
         // Impossible case 
-        if((long long)m*k > n){
+        if((long long)m * k > n){
             return -1;
         }
 
-        int low = *min_element(bloomDay.begin(), bloomDay.end());
-        int high = *max_element(bloomDay.begin(), bloomDay.end());
+        int left = *min_element(bloomDay.begin(), bloomDay.end());
+        int right = *max_element(bloomDay.begin(), bloomDay.end());
         int ans = -1;
 
-        while(low <= high){
+        while(left <= right){
+            int mid = (left + right)/2;
 
-            int mid = (low+high)/2;
-
-            if(canMake(bloomDay, m,k, mid)){
+            if(canMake(bloomDay , m , k, mid)){
                 ans = mid;
-                high = mid-1;
+                right = mid-1;
             }else{
-                low = mid+1;
+                left = mid+1;
             }
         }
 
