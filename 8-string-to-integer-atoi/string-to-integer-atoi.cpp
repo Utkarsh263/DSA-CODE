05ec315004edcb09a6f4710759c512a25369b0bc
Utkarsh <utkarshkohli821@gmail.com>
2026-01-29 22:19:25 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (21.77%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int i=0;
+        int i = 0;
         int n = s.size();
+        long result = 0;
         int sign = 1;
-        long result =0;
 
-        // Skip Leading Spaces 
-        while(i < n && s[i] == ' '){
+        // Skip Leading spaces
+        while(i<n && s[i]==' '){
             i++;
         }
 
-        // Check Sign 
-        if(i<n && (s[i]=='+' ||s[i]=='-')){
+        // Check for sign 
+        if(i<n && (s[i]=='+' || s[i]=='-')){
             if(s[i]=='-'){
                 sign = -1;
             }
             i++;
         }
 
+        // Check if next digit is number or character 
         if(i<n && !isdigit(s[i])){
             return 0;
         }
 
-        // Convert digit 
+        // Formulate the result 
         while(i<n && isdigit(s[i])){
             result = result*10 + (s[i]-'0');
 
@@ -31,10 +32,9 @@ public:
                 return INT_MAX;
             }
 
-            if(sign==-1 && -result < INT_MIN){
+            if(sign == -1 && -result < INT_MIN){
                 return INT_MIN;
             }
-
             i++;
         }
 
