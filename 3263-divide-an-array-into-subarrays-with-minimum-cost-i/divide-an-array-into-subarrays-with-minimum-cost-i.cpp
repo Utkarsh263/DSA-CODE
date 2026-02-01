c8bef55bcbd083a14c0204af9a99b29cd73ebae8
Utkarsh <utkarshkohli821@gmail.com>
2026-02-01 11:25:29 +0530

    Time: 0 ms (100.00%) | Memory: 30.6 MB (17.09%) - LeetSync

---
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& nums) {
+        int first = nums[0];
+
+        int smallest = INT_MAX;
+        int sec_smallest = INT_MAX;
+
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i] < smallest){
+                sec_smallest = smallest;
+                smallest = nums[i];
+            }else if(nums[i] < sec_smallest){
+                sec_smallest = nums[i];
+            }
+        }
+
+        return first + smallest + sec_smallest;
+    }
+};
