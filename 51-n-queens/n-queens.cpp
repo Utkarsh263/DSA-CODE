fc0794f1f81498cff4bf0772c64080c4e2f12a89
Utkarsh <utkarshkohli821@gmail.com>
2026-02-01 14:17:23 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (47.04%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
-    vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> result;
-        vector<string> board(n, string(n, '.'));
-        
-        unordered_set<int> cols;       // columns
-        unordered_set<int> diag;       // (row - col)
-        unordered_set<int> antiDiag;   // (row + col)
-        
-        backtrack(0, n, board, result, cols, diag, antiDiag);
-        return result;
-    }
-    
-    void backtrack(int row, int n, vector<string>& board, vector<vector<string>>& result,
-                   unordered_set<int>& cols, unordered_set<int>& diag, unordered_set<int>& antiDiag) {
-        if (row == n) {
+    vector<vector<string>>result;
+    vector<string>board;
+    vector<bool>cols, diag1, diag2;
+    int n;
+
+    void backtrack(int row){
+        if(row==n){
             result.push_back(board);
             return;
         }
-        
-        for (int col = 0; col < n; ++col) {
-            if (cols.count(col) || diag.count(row - col) || antiDiag.count(row + col))
-                continue; // Not safe
-            
-            // Place queen
+
+        for(int col=0; col<n; col++){
+            if(cols[col] || diag1[row-col+n-1] || diag2[row+col]){
+                continue;
+            }
+
             board[row][col] = 'Q';
-            cols.insert(col);
-            diag.insert(row - col);
-            antiDiag.insert(row + col);
-            
-            backtrack(row + 1, n, board, result, cols, diag, antiDiag);
-            
-            // Backtrack
-            board[row][col] = '.';
-            cols.erase(col);
-            diag.erase(row - col);
-            antiDiag.erase(row + col);
+            cols[col] = diag1[row-col+n-1] = diag2[row+col] = true;
+
+            backtrack(row+1);
+
+            board[row][col] ='.';
+            cols[col] =  diag1[row-col+n-1] = diag2[row+col] = false;
+
         }
+    }
+    vector<vector<string>> solveNQueens(int N) {
+        n=N;
+        board = vector<string>(n , string(n, '.'));
+        cols = vector<bool>(n , false);
+        diag1 = vector<bool>(2*n-1 , false);
+        diag2 = vector<bool>(2*n-1 , false);
+
+        backtrack(0);
+        return result;
     }
 };
