c546eebcff4feb8b69d2b6a032ee0a45d40f1256
Utkarsh <utkarshkohli821@gmail.com>
2026-02-02 16:24:50 +0530

    Time: 0 ms (100.00%) | Memory: 30.5 MB (73.87%) - LeetSync

--- 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.cpp
@@ -2,36 +2,33 @@ class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
         int n = nums.size();
-        int left =0, right = n-1;
-        // Find the leftmost boundary 
+        int left = 0, right = n-1;
 
         while(left < n-1 && nums[left] <= nums[left+1]){
             left++;
         }
 
-        if(left== n-1){
+        if(left == n-1){
             return 0;
         }
 
-        // Find the rightmost boundary 
-
-        while(right > 0 && nums[right] >= nums[right-1]){
+        while(right >0 && nums[right] >= nums[right-1]){
             right--;
         }
 
-        int subMin = INT_MAX;
-        int subMax = INT_MIN;
+        int subMin = INT_MAX , subMax = INT_MIN;
 
-        for(int i=left ; i<= right; i++){
-            subMin = min(subMin, nums[i]);
-            subMax = max(subMax, nums[i]);
+        for(int i=left; i<=right; i++){
+            subMin = min(subMin , nums[i]);
+            subMax = max(subMax , nums[i]);
         }
+
 
         while(left > 0 && nums[left-1] > subMin){
             left--;
         }
 
-        while(right <n-1 && nums[right+1] < subMax){
+        while(right < n-1 && nums[right+1] < subMax){
             right++;
         }
 
