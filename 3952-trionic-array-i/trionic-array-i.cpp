b689de82f59ca726647c846eee130e483110e391
Utkarsh <utkarshkohli821@gmail.com>
2026-02-03 16:44:39 +0530

    Time: 0 ms (100.00%) | Memory: 27.8 MB (10.15%) - LeetSync

---
+++ 3952-trionic-array-i/trionic-array-i.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isTrionic(vector<int>& nums) {
+        int n = nums.size();
+
+        if(n < 4){
+            return false;
+        }
+
+        int i =1;
+        // Increasing phase 
+        while(i<n && nums[i] > nums[i-1]){
+            i++;
+        }
+        if(i==1){
+            return false;
+        }
+
+        // Strictly Decreasing phase
+
+        while(i<n && nums[i] < nums[i-1]){
+            i++;
+        }
+
+        if(i==n || nums[i-1] >= nums[i-2]){
+            return false;
+        }
+
+        while(i<n && nums[i] > nums[i-1]){
+            i++;
+        }
+
+        return i==n;
+    }
+};
