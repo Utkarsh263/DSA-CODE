cbf9a65e3d2aca1047b6abd0afc1b7cb1d1c78b9
Utkarsh <utkarshkohli821@gmail.com>
2026-02-04 18:29:23 +0530

    Time: 0 ms (100.00%) | Memory: 22.2 MB (39.88%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -4,35 +4,31 @@ public:
         stack<int>st;
 
         for(int a : asteroids){
-
             if(a>0){
                 st.push(a);
             }else{
                 bool destroyed = false;
-
-                while(!st.empty() && st.top() > 0){
-                    if(st.top() < -a){
-                        st.pop();
+                while(!st.empty() && st.top() >0){
+                    if(st.top() > -a){
+                        destroyed = true;
+                        break;
                     }else if(st.top()==-a){
                         st.pop();
                         destroyed = true;
                         break;
                     }else{
-                        destroyed = true;
-                        break;
+                        st.pop();
                     }
                 }
 
                 if(!destroyed){
                     st.push(a);
-
                 }
             }
         }
 
         vector<int>ans(st.size());
-
-        for(int i= ans.size()-1; i>=0; i--){
+        for(int i= st.size()-1; i>=0; i--){
             ans[i] = st.top();
             st.pop();
         }
