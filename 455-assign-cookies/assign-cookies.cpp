06bcea497833a03149949b91c036de4169eeb74a
Utkarsh <utkarshkohli821@gmail.com>
2026-02-11 00:57:57 +0530

    Time: 3 ms (94.60%) | Memory: 44.8 MB (77.41%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        sort(g.begin(), g.end()); // sort greed factors
-        sort(s.begin(), s.end()); // sort cookie sizes
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
 
-        int i = 0; // index for children
-        int j = 0; // index for cookies
+        int i=0;
+        int j=0;
+        int count =0;
 
-        // loop until either all children checked OR all cookies used
-        while (i < g.size() && j < s.size()) {
-            if (s[j] >= g[i]) {
-                // cookie j can satisfy child i
-                i++; // move to next child
+        while(i < g.size() && j<s.size()){
+            if(g[i] <= s[j]){
+                count++;
+                i++;
+                j++;
+            }else{
+                j++;
             }
-            // either way, we used cookie j
-            j++;
         }
-        return i; // number of satisfied children
+
+        return count;
+
     }
 };
