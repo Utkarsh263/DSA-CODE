b0dc77d88ef755a687bd1b98b59b815356fa848f
Utkarsh <utkarshkohli821@gmail.com>
2026-02-12 00:19:15 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (94.01%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> result;
-        int i=0;
+        
+        vector<vector<int>>ans;
+        int i=0; 
         int n = intervals.size();
 
-        // Adding the leftmost segment
-        while(i < n && intervals[i][1] < newInterval[0]){
-            result.push_back(intervals[i]);
+        while(i<n && intervals[i][1] < newInterval[0]){
+            ans.push_back(intervals[i]);
             i++;
         }
-        // Merge two overlapping intervals
-        while(i < n && intervals[i][0] <= newInterval[1]){
-            newInterval[0] = min(newInterval[0] , intervals[i][0]);
+
+        // merge overlapping one 
+        while(i<n && intervals[i][0] <= newInterval[1]){
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
             newInterval[1] = max(newInterval[1], intervals[i][1]);
             i++;
         }
-        result.push_back(newInterval);
 
-        // Adding right most segment 
+        ans.push_back(newInterval);
 
-        while(i< n ){
-            result.push_back(intervals[i]);
+        while(i<n){
+            ans.push_back(intervals[i]);
             i++;
         }
 
-        return result;
+        return ans;
     }
 };
