6f51f235054f7f0920928771b8d1d49ad347455c
Utkarsh <utkarshkohli821@gmail.com>
2026-03-16 22:33:00 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (64.31%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,20 +1,49 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int, int> freq;
-        int n = nums.size();
 
-        for(int i=0; i<nums.size(); i++){
-            freq[nums[i]]++;
-        }
+        int count1 = 0, count2 = 0;
+        int candidate1 = 0, candidate2 = 1;
 
-        vector<int> result;
-        for(auto &pair : freq){
-            if(pair.second > n/3){
-                result.push_back(pair.first);
+        for(int num : nums){
+
+            if(num == candidate1)
+                count1++;
+
+            else if(num == candidate2)
+                count2++;
+
+            else if(count1 == 0){
+                candidate1 = num;
+                count1 = 1;
+            }
+
+            else if(count2 == 0){
+                candidate2 = num;
+                count2 = 1;
+            }
+
+            else{
+                count1--;
+                count2--;
             }
         }
 
-        return result;
+        // verification step
+        count1 = count2 = 0;
+
+        for(int num : nums){
+            if(num == candidate1) count1++;
+            else if(num == candidate2) count2++;
+        }
+
+        vector<int> ans;
+
+        int n = nums.size();
+
+        if(count1 > n/3) ans.push_back(candidate1);
+        if(count2 > n/3) ans.push_back(candidate2);
+
+        return ans;
     }
 };
