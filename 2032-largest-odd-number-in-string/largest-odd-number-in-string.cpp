bd4e858a7bd8f35b32daf69b05bf292800eae76b
Utkarsh <utkarshkohli821@gmail.com>
2026-03-17 22:36:19 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (37.88%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -5,7 +5,7 @@ public:
         int n = num.size();
 
         for(int i=n-1; i>=0; i--){
-            if(num[i] % 2 == 1){
+            if(num[i] % 2 != 0){
                 return num.substr(0, i+1);
             }
         }
