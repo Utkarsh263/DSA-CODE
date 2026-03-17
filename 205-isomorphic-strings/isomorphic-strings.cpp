7f7a30c192cb7e6a36ff5e63d4fc6fcc069ca20e
Utkarsh <utkarshkohli821@gmail.com>
2026-03-17 23:30:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (27.46%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -3,12 +3,14 @@ public:
     bool isIsomorphic(string s, string t) {
         
         unordered_map<char, char>ST;
-        unordered_map<char , char>TS;
+        unordered_map<char, char>TS;
 
         for(int i=0; i<s.size(); i++){
 
             char c1 = s[i];
             char c2 = t[i];
+
+            // Adding violating conditions
 
             if(ST.count(c1) && ST[c1] != c2){
                 return false;
@@ -17,6 +19,8 @@ public:
             if(TS.count(c2) && TS[c2] != c1){
                 return false;
             }
+
+            //Adding character to the map 
 
             ST[c1] = c2;
             TS[c2] = c1;
