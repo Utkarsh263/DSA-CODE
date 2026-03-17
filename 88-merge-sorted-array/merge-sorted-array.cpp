3b10a3708e4e4510cd6a5ed785b2a348169be53c
Utkarsh <utkarshkohli821@gmail.com>
2026-03-17 18:16:39 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (96.34%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -5,15 +5,13 @@ public:
         int j = n-1;
         int k = m+n-1;
 
-        while(i>= 0 && j>=0){
-            if(nums1[i] > nums2[j]){
-                nums1[k--] = nums1[i--];
-            }else{
+        while(i>=0 && j>=0){
+            if(nums1[i] < nums2[j]){
                 nums1[k--] = nums2[j--];
+            }else{
+                nums1[k--] = nums1[i--];
             }
         }
-
-        // Copy remaining elements 
 
         while(j>=0){
             nums1[k--] = nums2[j--];
