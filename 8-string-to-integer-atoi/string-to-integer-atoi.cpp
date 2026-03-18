e738b48921f6b7ae3ca3bd210d72f4b52b8c1a92
Utkarsh <utkarshkohli821@gmail.com>
2026-03-18 13:26:27 +0530

    Time: 3 ms (14.24%) | Memory: 9.2 MB (54.81%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int i = 0;
+        
+        int i=0;
         int n = s.size();
-        long result = 0;
-        int sign = 1;
 
-        // Skip Leading spaces
-        while(i<n && s[i]==' '){
+        // Skip the leading spaces 
+
+        while(i<n && s[i] ==' '){
             i++;
         }
 
-        // Check for sign 
-        if(i<n && (s[i]=='+' || s[i]=='-')){
-            if(s[i]=='-'){
+        // Sign 
+        int sign = 1;
+        if(i<n && (s[i]=='+' || s[i] =='-')){
+            if(s[i] == '-'){
                 sign = -1;
             }
             i++;
         }
 
-        // Check if next digit is number or character 
-        if(i<n && !isdigit(s[i])){
-            return 0;
-        }
+        long result =0;
 
-        // Formulate the result 
         while(i<n && isdigit(s[i])){
-            result = result*10 + (s[i]-'0');
 
-            if(sign == 1 && result > INT_MAX){
+            result = result*10 +(s[i] -'0');
+
+            // Handle overflow 
+            if(sign*result > INT_MAX){
                 return INT_MAX;
             }
 
-            if(sign == -1 && -result < INT_MIN){
+            if(sign*result < INT_MIN){
                 return INT_MIN;
             }
+
             i++;
         }
 
-        return sign*result;
+        return sign * result;
     }
 };
