ad69e1bd4be09ffd565a388d22473ef805dbcc5c
Utkarsh <utkarshkohli821@gmail.com>
2026-03-18 13:54:53 +0530

    Time: 78 ms (85.39%) | Memory: 11.2 MB (49.15%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
     int beautySum(string s) {
+        
         int n = s.size();
-        int ans =0;
+        int total = 0;
 
-        for(int i=0; i<n ; i++){
-            vector<int> freq(26,0);
-            for(int j=i; j<n; j++){
-                freq[s[j]-'a']++;
+        for(int i = 0; i < n; i++) {
+            
+            vector<int> freq(26, 0);
 
-                int maxFreq =0;
+            int maxFreq = 0;
+
+            for(int j = i; j < n; j++) {
+                
+                // Expand window
+                freq[s[j] - 'a']++;
+
+                // Update max freq
+                maxFreq = max(maxFreq, freq[s[j] - 'a']);
+
+                // Find min freq (only non-zero)
                 int minFreq = INT_MAX;
 
-                for(int k=0; k<26; k++){
-                    if(freq[k] >0){
-                        maxFreq = max(maxFreq , freq[k]);
-                        minFreq = min(minFreq , freq[k]);
+                for(int k = 0; k < 26; k++) {
+                    if(freq[k] > 0) {
+                        minFreq = min(minFreq, freq[k]);
                     }
                 }
 
-                ans += (maxFreq - minFreq);
+                total += (maxFreq - minFreq);
             }
         }
 
-        return ans;
+        return total;
     }
 };
