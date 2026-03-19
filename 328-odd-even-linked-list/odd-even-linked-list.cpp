dcbfd06e49568dd2a60d52424cfb35023d26381b
Utkarsh <utkarshkohli821@gmail.com>
2026-03-19 22:56:16 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (7.61%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -8,31 +8,29 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
- // Instead of creating new nodes, play with pointers 
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
+        
         if(head == NULL || head->next == NULL){
             return head;
         }
+
         ListNode* odd = head;
         ListNode* even = head->next;
         ListNode* evenHead = even;
 
         while(even != NULL && even->next != NULL){
 
-            //reconnect pointer
             odd->next = even->next;
-            // Iterate
-            odd= odd->next;
+            odd = odd->next;
 
-            // Similarly for Even indices
             even->next = odd->next;
             even = even->next;
         }
 
         odd->next = evenHead;
+
         return head;
     }
 };
