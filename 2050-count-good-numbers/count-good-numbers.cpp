3b1e77bd2efa94c2d02cfbf5f1ef794e91f4de36
Utkarsh <utkarshkohli821@gmail.com>
2026-03-21 23:28:34 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (36.59%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -2,26 +2,27 @@ class Solution {
 public:
     long long mod = 1e9+7;
 
-    long long power(long long a, long long b){
+    long long power(long long a , long long b){
 
         long long res = 1;
-        while(b>0){
-            if(b%2 == 1){
-                res = (res*a) % mod;
+
+        while(b > 0){
+
+            if(b % 2 == 1){
+                res = (res * a)% mod;
             }
 
-            a = (a*a)%mod;
+            a = (a*a) % mod;
             b /= 2;
-
         }
 
         return res;
     }
     int countGoodNumbers(long long n) {
         
-        long long evenCount = (n+1)/2;
         long long oddCount = n/2;
+        long long evenCount = (n+1)/2;
 
-        return (power(5, evenCount) * power(4, oddCount)) % mod;
+        return (power(5, evenCount)* power(4 , oddCount)) % mod;
     }
 };
