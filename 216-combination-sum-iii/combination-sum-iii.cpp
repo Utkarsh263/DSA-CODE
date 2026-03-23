2f1c00e6c8083b10f6dbef2130cc3576ae965652
Utkarsh <utkarshkohli821@gmail.com>
2026-03-23 23:26:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (59.50%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    void backtrack(int start, int k , int n , vector<int>&path, vector<vector<int>>&result){
-        if(path.size()==k && n==0){
-            result.push_back(path);
+    vector<vector<int>>ans;
+    vector<int>temp;
+
+    void solve(int start , int k , int target){
+
+        if(temp.size() == k){
+            if(target == 0){
+                ans.push_back(temp);
+            }
             return;
         }
 
-        if(path.size() > k || n <0){
+        if(target < 0){
             return;
         }
 
         for(int i=start; i<=9; i++){
-            path.push_back(i);
-            backtrack(i+1,k, n-i,path, result);
-            path.pop_back();
+            temp.push_back(i);
+            solve(i+1 , k, target-i);
+            temp.pop_back();
         }
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int>path;
-        vector<vector<int>>result;
-        backtrack(1, k , n , path, result);
-        return result;
+        
+        solve(1, k, n);
+        return ans;
     }
 };
