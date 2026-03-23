aa29488859761d3a7c9d84e0f2327f981e64e191
Utkarsh <utkarshkohli821@gmail.com>
2026-03-23 23:18:28 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (30.48%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    void backtrack(int index, vector<int>&nums, vector<int>&path, vector<vector<int>>&ans){
-        ans.push_back(path);
+    vector<int>temp;
+    vector<vector<int>>ans;
 
-        for(int i=index; i<nums.size(); i++){
+    void solve(int start , vector<int>&nums){
 
-            if(i>index && nums[i]==nums[i-1]){
+        ans.push_back(temp);
+
+        for(int i=start; i<nums.size(); i++){
+
+            if(i > start && nums[i] == nums[i-1]){
                 continue;
             }
 
-            // Include in subset
-            path.push_back(nums[i]);
-            backtrack(i+1, nums, path, ans);
-            path.pop_back();
+            temp.push_back(nums[i]);
+            solve(i+1 , nums);
+            temp.pop_back();
         }
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        
         sort(nums.begin(), nums.end());
-        vector<int>path;
-        vector<vector<int>>ans;
-        backtrack(0,nums, path, ans);
+        solve(0, nums);
         return ans;
     }
 };
