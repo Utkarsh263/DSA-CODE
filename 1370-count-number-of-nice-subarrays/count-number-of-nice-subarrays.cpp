d27d37d87ffc31c2180c4fe3d6a0a0374250cc7a
Utkarsh <utkarshkohli821@gmail.com>
2026-03-31 00:09:49 +0530

    Time: 15 ms (31.68%) | Memory: 71.6 MB (47.58%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -3,12 +3,12 @@ public:
     int atMost(vector<int>&nums , int k){
 
         int left =0;
-        int count =0;
         int oddCount =0;
+        int count =0;
 
         for(int right =0; right < nums.size(); right++){
 
-            if(nums[right] % 2 == 1){
+            if(nums[right] % 2 ==1){
                 oddCount++;
             }
 
@@ -19,7 +19,7 @@ public:
                 left++;
             }
 
-            count += (right - left +1);
+            count += (right-left+1);
         }
 
         return count;
