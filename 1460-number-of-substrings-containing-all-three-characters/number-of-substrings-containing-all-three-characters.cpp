185637aa34aaa14225d84dacdf7510c7768191a4
Utkarsh <utkarshkohli821@gmail.com>
2026-03-30 16:35:35 +0530

    Time: 5 ms (71.82%) | Memory: 11.1 MB (56.40%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        vector<int>count(3, 0); // for a , b , c
+        
+        // Array to store count of each character
+        vector<int>count(3,0);
         int left =0;
-        int ans = 0;
+        int ans =0;
         int n = s.size();
 
-        for(int right =0; right < n; right++){
-            //increment the count of character
+        for(int right =0; right < n ; right++){
+
+            // Increment the count of each character 
             count[s[right]-'a']++;
 
-            // when window is valid 
-
-            while(count[0] > 0 && count[1] > 0 && count[2]> 0){
+            //Make window valid 
+            while(count[0] > 0 && count[1] > 0 && count[2] > 0){
 
                 ans += (n-right);
 
-                //start removing characters from the left 
+                // start shrinking the window 
                 count[s[left]-'a']--;
                 left++;
             }
