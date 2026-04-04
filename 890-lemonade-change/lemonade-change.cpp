9176f03326a2208e84ac854c087331804777b506
Utkarsh <utkarshkohli821@gmail.com>
2026-04-04 23:49:22 +0530

    Time: 0 ms (100.00%) | Memory: 87.2 MB (75.82%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        int five =0;
-        int ten=0;
+        
+        int count5 = 0;
+        int count10 = 0;
 
-        for(int x : bills){
-
-            if(x==5){
-                five++;
-            }else if(x==10){
-                if(five==0){
+        for(int bill : bills){ 
+            if(bill == 5){ // As no change is required
+                count5++;
+            }else if(bill == 10){ // 10 -5 = 5 needs to be returned
+                if(count5 > 0){
+                    count5--;
+                    count10++;
+                }else{
                     return false;
                 }
-                ten++;
-                five--;
             }else{
-                if(five>0 && ten > 0){
-                    five--;
-                    ten--;
-                }else if(five >=3){
-                    five-=3;
+                if(count10 > 0 && count5 > 0){ // As 20 has been given , so 15 needs to be returned either 3*5  or 10+5
+                    count10--;
+                    count5--;
+                }else if(count5 >= 3){
+                    count5 -= 3;
                 }else{
                     return false;
                 }
@@ -27,5 +28,6 @@ public:
         }
 
         return true;
+
     }
 };
