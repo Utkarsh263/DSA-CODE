b1ae7812ac1e465a04def7be6c3eea3e9fe611a6
Utkarsh <utkarshkohli821@gmail.com>
2026-04-05 14:17:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (75.62%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -2,31 +2,31 @@ class Solution {
 public:
     bool checkValidString(string s) {
         
-        int low = 0;  // Minimum opening characters
-        int high = 0; // Maximum opening characters
+        int low =0;
+        int high = 0;
 
         for(char c : s){
 
-            if(c == '('){
+            if(c=='('){
                 low++;
                 high++;
-            }else if(c == ')'){
+            }else if(c==')'){
                 low--;
-                high--;  // As to balance closing bracket , opening bracket will be used 
+                high--;
             }else{
                 low--;
                 high++;
             }
 
-            if(low < 0){
-                low = 0;  // Minimum opening characters count never be negative 
+            if(high < 0){
+                return false;
             }
 
-            if(high < 0){
-                return false;  // Maximum opening character if becomes negative  => invalid 
+            if(low < 0){
+                low = 0;
             }
         }
 
-        return low == 0; // Will only be true if low == 0 as all opening bracket will be balanced by closing brackets 
+        return low == 0;
     }
 };
