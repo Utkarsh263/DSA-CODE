7862c13401a79681a5a31bda17fb62c3a1760a55
Utkarsh <utkarshkohli821@gmail.com>
2026-04-05 23:51:34 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (69.26%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
+        
         int n = ratings.size();
-        vector<int> candies(n, 1);  // Step 1: everyone gets 1
+        vector<int>result(n , 1);
 
-        // Step 2: Left → Right
-        for(int i = 1; i < n; i++) {
-            if(ratings[i] > ratings[i - 1]) {
-                candies[i] = candies[i - 1] + 1;
+        // Left to Right scan 
+
+        for(int i=1; i<n; i++){
+            if(ratings[i] > ratings[i-1]){
+                result[i] = result[i-1] + 1;
             }
         }
 
-        // Step 3: Right → Left
-        for(int i = n - 2; i >= 0; i--) {
-            if(ratings[i] > ratings[i + 1]) {
-                candies[i] = max(candies[i], candies[i + 1] + 1);
+        // Left to Right Scan 
+        for(int i=n-2; i>=0; i--){
+            if(ratings[i] > ratings[i+1]){
+                result[i] = max(result[i] , result[i+1] + 1);
             }
         }
 
-        // Step 4: Sum all candies
         int total = 0;
-        for(int c : candies) {
+        for(int c : result){
             total += c;
         }
 
