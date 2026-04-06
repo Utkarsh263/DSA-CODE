482164f466c2903fc7835c0431960cc7711cde86
Utkarsh <utkarshkohli821@gmail.com>
2026-04-06 22:03:38 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (14.12%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -9,47 +9,30 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// class Solution {
-// public:
-//     void inorder(TreeNode* root , vector<int>&ans){
-//         if(!root){
-//             return;
-//         }
-//         inorder(root->left, ans);
-//         ans.push_back(root->val);
-//         inorder(root->right , ans);
-//     }
-//     vector<int> inorderTraversal(TreeNode* root) {
-//         vector<int>ans;
-//         inorder(root, ans);
-//         return ans;
-//     }
-// };
-
-// Iterative version of traversal
-
-class Solution{
+class Solution {
 public:
-    vector<int> inorderTraversal(TreeNode* root){
-        vector<int> ans;
-        stack<TreeNode*> st;
+    vector<int> inorderTraversal(TreeNode* root) {
+        
+        vector<int>result;
+        stack<TreeNode*>st;
         TreeNode* curr = root;
 
-        while(true){
-            if(curr!= NULL){
-                st.push(curr);
-                curr= curr->left;
-            }else{
-                if(st.empty()){
-                    break;
-                }
-                curr= st.top();
-                st.pop();
-                ans.push_back(curr->val);
-                curr= curr->right;
-            }
-        }
-        return ans;
-    }
+        while(!st.empty() || curr != NULL){
 
+            // Go left as much as possible
+
+            while(curr != NULL){
+                st.push(curr);
+                curr = curr->left;
+            }
+
+            curr = st.top();
+            st.pop();
+            result.push_back(curr->val);
+
+            curr = curr->right;
+        }
+
+        return result;
+    }
 };
