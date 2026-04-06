baefcb44229115f764d7ab5854074e7494ba8651
Utkarsh <utkarshkohli821@gmail.com>
2026-04-06 13:50:41 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (16.42%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -1,47 +1,32 @@
-// class Solution{
-// public:
-//     vector<int>postorderTraversal(TreeNode* root){
-//         vector<int> postorder;
-//         if(root == NULL){
-//             return postorder;
-//         }
-//         stack<TreeNode*> st1, st2;
-//         st1.push(root);
-//         while(!st1.empty()){
-//             root = st1.top();
-//             st1.pop();
-//             st2.push(root);
-
-//             if(root->left != NULL){
-//                 st1.push(root->left);
-//             }
-
-//             if(root->right != NULL){
-//                 st1.push(root->right);
-//             }
-//         }
-
-//         while(!st2.empty()){
-//             postorder.push_back(st2.top()->val);
-//             st2.pop();
-//         }
-//         return postorder;
-//     }
-// };
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void postorder(TreeNode* root , vector<int> &ans){
-        if(!root){
+    void postOrder(TreeNode* root , vector<int>&result){
+
+        if(root == NULL){
             return;
         }
-        postorder(root->left , ans);
-        postorder(root->right , ans);
-        ans.push_back(root->val);
+
+        // PostOrder Traversals => LEFT RIGHT NODE 
+
+        postOrder(root->left , result);
+        postOrder(root->right , result);
+        result.push_back(root->val);
+
     }
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        postorder(root, ans);
-        return ans;
+        vector<int>result;
+        postOrder(root , result);
+        return result;
     }
 };
