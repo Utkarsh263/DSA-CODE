a5b6ac93aec7c8a1d72272971c6159ee7864df10
Utkarsh <utkarshkohli821@gmail.com>
2026-04-12 11:32:51 +0530

    Time: 7 ms (5.63%) | Memory: 61.3 MB (7.65%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -11,27 +11,34 @@
  */
 class Solution {
 public:
-    TreeNode* first = NULL , *second = NULL , *prev = NULL;
-    void inorder(TreeNode* root){
-        if(root == NULL){
-            return;
-        }
+    vector<TreeNode*> nodes;
+
+    void inorder(TreeNode* root) {
+        if (!root) return;
+
         inorder(root->left);
-
-        if(prev && prev->val > root->val){
-            if(first == NULL){
-                first = prev;
-            }
-            second = root;
-        }
-        prev = root;
-
+        nodes.push_back(root);   // store NODE, not value
         inorder(root->right);
     }
+
     void recoverTree(TreeNode* root) {
         inorder(root);
-        if(first && second){
-            swap(first->val , second->val);
+
+        TreeNode* first = NULL;
+        TreeNode* second = NULL;
+
+        for (int i = 1; i < nodes.size(); i++) {
+            if (nodes[i-1]->val > nodes[i]->val) {
+
+                if (!first) {
+                    first = nodes[i-1];  // bigger wrong
+                }
+
+                second = nodes[i];       // smaller wrong
+            }
         }
+
+        // actually fix tree
+        swap(first->val, second->val);
     }
 };
