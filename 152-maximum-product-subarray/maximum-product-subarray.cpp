f50e68536e7855e354c98896fd1709c3fddf54ec
Utkarsh <utkarshkohli821@gmail.com>
2026-04-24 00:51:00 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (59.39%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -6,9 +6,8 @@ public:
         int minEnd = nums[0];
         int ans = nums[0];
 
-        int n = nums.size();
+        for(int i=1; i<nums.size(); i++){
 
-        for(int i=1; i<n; i++){
             int curr = nums[i];
 
             int tempMax = max({curr , curr*maxEnd , curr*minEnd});
