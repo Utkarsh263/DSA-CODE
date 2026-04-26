bfdc9a8a584cec0158ab373ae49949693333953e
Utkarsh <utkarshkohli821@gmail.com>
2026-04-27 00:39:29 +0530

    Time: 0 ms (100.00%) | Memory: 27.9 MB (99.00%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int candidate =0;
-        int count=0;
+        
+        // Boyee Moore Algorithm 
+        int count = 0;
+        int candidate = 0;
 
-        for(int num : nums){
-            if(count==0){
-                candidate = num;
+        for(int i=0; i<nums.size(); i++){
+
+            if(count ==0){
+                candidate = nums[i];
             }
 
-            if(candidate == num){
+            if(nums[i] == candidate){
                 count++;
             }else{
                 count--;
