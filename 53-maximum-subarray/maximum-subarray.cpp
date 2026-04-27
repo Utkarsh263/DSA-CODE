6079292979deee96ea9ca8a868847a1110d2861a
Utkarsh <utkarshkohli821@gmail.com>
2026-04-27 11:17:28 +0530

    Time: 4 ms (11.24%) | Memory: 71.8 MB (51.68%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -4,18 +4,19 @@ public:
         
         int n = nums.size();
         int currSum = 0;
-        int totalSum = INT_MIN;
+        int maxSum = INT_MIN;
 
         for(int i=0; i<n; i++){
+
             if(currSum < 0){
                 currSum = 0;
             }
 
             currSum += nums[i];
 
-            totalSum = max(totalSum , currSum);
+            maxSum = max(maxSum , currSum);
         }
 
-        return totalSum;
+        return maxSum;
     }
 };
