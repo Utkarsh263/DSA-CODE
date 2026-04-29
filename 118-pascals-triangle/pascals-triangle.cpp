4525e9b37d049193fcae737931bea910100ef622
Utkarsh <utkarshkohli821@gmail.com>
2026-04-29 22:48:49 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (89.06%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -5,11 +5,10 @@ public:
         vector<vector<int>>result;
 
         for(int i=0; i<numRows; i++){
+            vector<int>row(i+1 ,  1);
 
-            vector<int>row(i+1, 1);
-
-            for(int j=1 ; j<i; j++){
-                row[j] = result[i-1][j-1]+result[i-1][j];
+            for(int j=1; j<i; j++){
+                row[j] = result[i-1][j-1] + result[i-1][j];
             }
 
             result.push_back(row);
