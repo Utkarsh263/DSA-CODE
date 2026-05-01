fd7d02a7d48da167c9f863c09c09a4b2c5fad535
Utkarsh <utkarshkohli821@gmail.com>
2026-05-01 12:37:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (27.57%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -2,29 +2,30 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         
-        // If both string length differs 
+        // f string length differs 
         if(s.length() != t.length()){
             return false;
         }
 
-        // s->t mapping 
-        unordered_map<char , char>sT;
+        unordered_map<char, char>sT;
         unordered_map<char, char>tS;
 
         for(int i=0; i<s.size(); i++){
 
-            // check for s->t mapping 
+            // check for s->t mapping
+
             if(sT.count(s[i]) && sT[s[i]] != t[i]){
                 return false;
             }
 
-            // check for t->s mapping 
+            // check for t->s mapping
             if(tS.count(t[i]) && tS[t[i]] != s[i]){
                 return false;
             }
 
+            // Add the mapping 
             sT[s[i]] = t[i];
-            tS[t[i]] = s[i]; // Mapped the elements
+            tS[t[i]] = s[i];
         }
 
         return true;
