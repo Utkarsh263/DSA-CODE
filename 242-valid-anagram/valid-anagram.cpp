57b7d3e9cbf27cb00b3f2184593597d3db91791a
Utkarsh <utkarshkohli821@gmail.com>
2026-05-01 19:59:37 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (74.68%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        
+
+        // If string length are different 
+
         if(s.length() != t.length()){
             return false;
         }
 
+        // Crreating the vector of size 26 freq 
         vector<int>freq(26, 0);
 
-        for(int i=0; i<s.size();i++){
 
-            // Add character to the frequency map 
+        //Incrementing  the frequency 
+        for(int i=0; i<s.size();i++){
             freq[s[i]-'a']++;
         }
 
-        for(int i=0; i<t.size();i++){
-            // Decrease the frequency
+        //Decrementing the frequency from string t 
+        for(int i=0; i<t.size(); i++){
             freq[t[i]-'a']--;
         }
 
+        // Checking the validity 
         for(int i=0; i<26; i++){
             if(freq[i] != 0){
                 return false;
