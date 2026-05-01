b2151b079dc0b30b8c092ccf4afa3dd3808d5cef
Utkarsh <utkarshkohli821@gmail.com>
2026-05-01 19:52:13 +0530

    Time: 1 ms (14.89%) | Memory: 8.1 MB (61.73%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool rotateString(string s, string goal) {
         
+        // If sizes are of different sizes 
         if(s.size() != goal.size()){
             return false;
         }
