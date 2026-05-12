c33464535da88c6bcd0fd5e9c675cfda9fae2972
Utkarsh <utkarshkohli821@gmail.com>
2026-05-12 18:22:57 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (40.26%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -16,11 +16,8 @@ public:
         ListNode* curr = head;
 
         while(curr != NULL){
-
             ListNode* next = curr->next;
-
             curr->next = prev;
-
             prev = curr;
             curr = next;
         }
