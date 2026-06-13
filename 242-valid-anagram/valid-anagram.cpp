bfb0b7fccf60f4e01bad49d33afac9bd6abb74b5
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-13 14:18:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (49.03%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-
-        // If string length are different 
-
-        if(s.length() != t.length()){
-            return false;
-        }
-
-        // Crreating the vector of size 26 freq 
+        
+        // Creating the vector array of each letter of size 26 with freq equals to zero
         vector<int>freq(26, 0);
 
-
-        //Incrementing  the frequency 
-        for(int i=0; i<s.size();i++){
+        for(int i=0; i<s.size(); i++){
             freq[s[i]-'a']++;
         }
 
-        //Decrementing the frequency from string t 
         for(int i=0; i<t.size(); i++){
-            freq[t[i]-'a']--;
+            freq[t[i] - 'a']--;
         }
 
-        // Checking the validity 
         for(int i=0; i<26; i++){
             if(freq[i] != 0){
                 return false;
