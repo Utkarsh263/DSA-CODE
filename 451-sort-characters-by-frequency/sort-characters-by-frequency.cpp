3cdc01425e0006624576885844d236dbbef79a52
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-13 15:36:25 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (38.48%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -2,24 +2,21 @@ class Solution {
 public:
     string frequencySort(string s) {
         
-        // We will use hashmap to store frequency and heap for max character 
+        unordered_map<char , int>freq;
 
-        unordered_map<char, int>freq;
-
-        for(int i=0; i<s.size(); i++){
+        for(int i=0; i<s.length(); i++){
             freq[s[i]]++;
         }
 
-        string result = "";
-
-        // Building max heap 
-        priority_queue<pair<int, char>>pq;
+        priority_queue<pair<int,char>>pq;
 
         for(auto it : freq){
-            pq.push({it.second, it.first});
+            pq.push({it.second , it.first});
         }
 
-        // Process the max heap till empty 
+        // Process the heap 
+        string result = "";
+
         while(!pq.empty()){
             auto it = pq.top();
             pq.pop();
