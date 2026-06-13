73c5acf4004b1d278135645df3c984a1735f4bab
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-13 18:42:51 +0530

    Time: 4 ms (5.97%) | Memory: 9.1 MB (54.23%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -4,36 +4,33 @@ public:
         
         int i=0;
         int n = s.size();
+        long long result = 0;
+        int sign = 1;
 
-        // Skip leading spaces 
         while(i<n && s[i] == ' '){
             i++;
         }
 
-        // Determine sign 
-        int sign = 1;
         if(i<n && (s[i] == '+' || s[i] == '-')){
-            if(s[i] =='-'){
+            if(s[i] == '-'){
                 sign = -1;
             }
             i++;
         }
 
-        long long result = 0;
-
         while(i<n && isdigit(s[i])){
 
-           result = result*10 + (s[i] -'0');
+            int digit = s[i] -'0';
+            result = (result*10)+digit;
 
-           if(sign * result > INT_MAX){
-                return INT_MAX;
-           }
-
-           if(sign * result < INT_MIN){
+            if(sign * result < INT_MIN){
                 return INT_MIN;
-           }
+            }
 
-           i++;
+            if(sign * result > INT_MAX){
+                return INT_MAX;
+            }
+            i++;
         }
 
         return sign*result;
