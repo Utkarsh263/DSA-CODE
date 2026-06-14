560a87615ef99d3f273b38a43036d48f66a8d854
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-14 21:58:08 +0530

    Time: 7 ms (24.79%) | Memory: 18.1 MB (20.68%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    void solve(int idx, vector<int>&candidates , int target , vector<int>&path , vector<vector<int>>&result){
+    void solve(int idx , vector<int>&candidates , int target , vector<int>&path , vector<vector<int>>&result){
 
         if(idx == candidates.size()){
             if(target == 0){
                 result.push_back(path);
-                
             }
             return;
         }
 
-        // Exclusion case 
-        solve(idx +1 , candidates , target , path , result);
+        // Exclusing the number 
+        solve(idx+1 , candidates , target , path , result);
 
-        if(candidates[idx] <= target){
+        // Including the number 
+        if(target  >= candidates[idx]){
             path.push_back(candidates[idx]);
             target = target - candidates[idx];
-            solve(idx, candidates , target , path, result);
+            solve(idx , candidates , target , path, result);
             path.pop_back();
         }
     }
@@ -24,7 +24,7 @@ public:
         vector<vector<int>>result;
         vector<int>path;
 
-        solve(0,candidates ,target, path , result);
+        solve(0, candidates , target , path , result);
 
         return result;
     }
