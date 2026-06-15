f3dc2dd619e16c840c49565a8d73c9513531ce73
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-15 23:47:30 +0530

    Time: 849 ms (15.36%) | Memory: 11.7 MB (18.94%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
     int rows, cols;
-    bool dfs(vector<vector<char>>& board, string &word, int r, int c , int idx){
+    bool dfs(int idx , vector<vector<char>>&board , string word, int r, int c){
 
+        // Base case 
         if(idx == word.size()){
             return true;
         }
 
-        if(r<0 || c<0 || r>= rows ||c >= cols || board[r][c] != word[idx]){
+        // False condition 
+        if (r < 0 || c < 0 ||r >= rows || c >= cols || board[r][c] != word[idx]) {
             return false;
         }
 
         char temp = board[r][c];
-        board[r][c] = '#'; // Marks as visited
+        board[r][c] = '#'; // Mark as visited 
 
-        bool found = 
-            dfs(board, word , r+1, c, idx+1) ||
-            dfs(board, word , r-1, c, idx+1) ||
-            dfs(board, word , r, c-1, idx+1) ||
-            dfs(board, word , r, c+1, idx+1) ;
-        
+        bool found = dfs(idx+1 , board, word , r+1 , c) || dfs(idx+1 , board, word , r-1 , c) || dfs(idx+1 , board, word , r , c+1) || dfs(idx+1 , board, word , r , c-1);
+
+        // Backtrack 
         board[r][c] = temp;
+
         return found;
 
     }
     bool exist(vector<vector<char>>& board, string word) {
+        
         rows = board.size();
         cols = board[0].size();
 
         for(int i=0; i<rows; i++){
             for(int j=0; j<cols; j++){
-                if(dfs(board, word, i, j, 0)){
+                if(dfs(0, board , word, i, j)){
                     return true;
                 }
             }
