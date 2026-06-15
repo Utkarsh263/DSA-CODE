c1ea631d96cba26e9110be76fd82dce1a039e276
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-15 10:48:08 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (82.00%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int n = nums.size();
+        
+        // If array is sorted and rotated so there will only be one point where sorted order breaks 
 
+        int n = nums.size();
         int count = 0;
+
         for(int i=1; i<n; i++){
-            if(nums[i-1] > nums[i]){
+
+            if(nums[i] < nums[i-1]){
                 count++;
             }
         }
+        // To check condition for the last element of the array 
         if(nums[n-1] > nums[0]){
             count++;
         }
 
-        return count <= 1;
+        return count<=1;
     }
 };
