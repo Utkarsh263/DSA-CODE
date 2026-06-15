2e1bc120823a3b49f3ca0667116590194dd23fd2
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-15 23:15:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (89.16%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
-    void backtrack(int index, string &digits, vector<string>&mp, string &curr, vector<string>&ans){
+    void solve(int idx, string &digits , string &path , vector<string>&result , vector<string>&phone){
 
-        if(index == digits.size()){
-            ans.push_back(curr);
+        if(idx == digits.size()){
+            result.push_back(path);
             return;
         }
 
-        string letters = mp[digits[index]-'0'];
+        string letters = phone[digits[idx] - '0'];
 
         for(char ch : letters){
-            curr.push_back(ch);
-            backtrack(index+1, digits, mp, curr, ans);
-            curr.pop_back();
+            path.push_back(ch);
+            solve(idx+1 , digits , path , result , phone);
+            path.pop_back();
         }
     }
     vector<string> letterCombinations(string digits) {
-        vector<string>ans;
-        string curr;
+        
+        if(digits.empty()){
+            return {};
+        }
 
-        vector<string> mp={
-            "","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"
+        vector<string> phone = {
+            "", "", "abc", "def",
+            "ghi", "jkl", "mno",
+            "pqrs", "tuv", "wxyz"
         };
 
-        backtrack(0, digits, mp, curr, ans);
-        return ans;
+        vector<string>result;
+        string path;
+
+        solve(0, digits, path, result , phone);
+
+        return result;
     }
 };
