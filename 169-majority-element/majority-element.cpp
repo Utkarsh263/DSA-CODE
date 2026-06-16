cc06158c9d30d71afc512d706f63f83b725f557a
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-16 12:26:18 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (87.15%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -6,11 +6,12 @@ public:
         int candidate = 0;
 
         for(int i=0; i<nums.size(); i++){
+
             if(count == 0){
                 candidate = nums[i];
             }
 
-            if(candidate == nums[i]){
+            if(nums[i] == candidate){
                 count++;
             }else{
                 count--;
