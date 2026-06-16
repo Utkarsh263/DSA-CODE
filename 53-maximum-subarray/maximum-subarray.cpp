f6baddeb7c54c5c4af1ee17880dfa16c11e86fbc
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-16 14:57:29 +0530

    Time: 3 ms (19.44%) | Memory: 71.8 MB (14.37%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -2,19 +2,22 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         
-        int n = nums.size();
+        // We can use Kadane Algorithm which is an optimization of PrefixSum approach 
+
         int currSum = 0;
         int maxSum = INT_MIN;
+        int n = nums.size();
 
         for(int i=0; i<n; i++){
 
+            currSum += nums[i];
+
+            
+
+            maxSum = max(maxSum , currSum);
             if(currSum < 0){
                 currSum = 0;
             }
-
-            currSum += nums[i];
-
-            maxSum = max(maxSum , currSum);
         }
 
         return maxSum;
