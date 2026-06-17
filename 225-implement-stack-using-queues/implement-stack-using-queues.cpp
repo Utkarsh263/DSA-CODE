ea70ceb9add29537c9aab4bbc3490e72a2ef4f78
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-17 19:00:28 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (5.22%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -1,33 +1,38 @@
 class MyStack {
 public:
-    queue<int>q;
+    // Using two queue 
+    queue<int>q1;
+    queue<int>q2;
     MyStack() {
         
     }
     
     void push(int x) {
-        
-        q.push(x);
+        while(!q1.empty()){
+            q2.push(q1.front());
+            q1.pop();
+        }
 
-        int size = q.size();
-        for(int i=0; i<size-1; i++){
-            q.push(q.front());
-            q.pop();
+        q1.push(x);
+
+        while(!q2.empty()){
+            q1.push(q2.front());
+            q2.pop();
         }
     }
     
     int pop() {
-        int val = q.front();
-        q.pop();
+        int val = q1.front();
+        q1.pop();
         return val;
     }
     
     int top() {
-        return q.front();
+        return q1.front();
     }
     
     bool empty() {
-        return q.empty();
+        return q1.empty();
     }
 };
 
@@ -39,3 +44,48 @@ public:
  * int param_3 = obj->top();
  * bool param_4 = obj->empty();
  */
+
+
+
+
+//  class MyStack {
+// public:
+//     queue<int>q;
+//     MyStack() {
+        
+//     }
+    
+//     void push(int x) {
+        
+//         q.push(x);
+
+//         int size = q.size();
+//         for(int i=0; i<size-1; i++){
+//             q.push(q.front());
+//             q.pop();
+//         }
+//     }
+    
+//     int pop() {
+//         int val = q.front();
+//         q.pop();
+//         return val;
+//     }
+    
+//     int top() {
+//         return q.front();
+//     }
+    
+//     bool empty() {
+//         return q.empty();
+//     }
+// };
+
+// /**
+//  * Your MyStack object will be instantiated and called as such:
+//  * MyStack* obj = new MyStack();
+//  * obj->push(x);
+//  * int param_2 = obj->pop();
+//  * int param_3 = obj->top();
+//  * bool param_4 = obj->empty();
+//  */
