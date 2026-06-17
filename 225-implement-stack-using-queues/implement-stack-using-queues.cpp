7ac4d82e44350d947a1322b4df58e46e2fd679ce
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-17 18:57:02 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (5.22%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -8,9 +8,9 @@ public:
     void push(int x) {
         
         q.push(x);
-        int n = q.size();
 
-        for(int i=0; i<n-1; i++){
+        int size = q.size();
+        for(int i=0; i<size-1; i++){
             q.push(q.front());
             q.pop();
         }
