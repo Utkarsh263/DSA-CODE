d4762714f4ffeec48f709506fe76a13cc6ffccb1
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-17 11:49:30 +0530

    Time: 0 ms (100.00%) | Memory: 97.4 MB (60.17%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -6,9 +6,10 @@ public:
         int maxProfit = 0;
 
         for(int i=0; i<prices.size(); i++){
-            
+
             minPrice = min(prices[i], minPrice);
             maxProfit = max(maxProfit , prices[i]-minPrice);
+
         }
 
         return maxProfit;
