bf1683327097238258ab0525c77156ad6e724dd2
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-17 22:15:43 +0530

    Time: 6 ms (40.08%) | Memory: 28.3 MB (91.28%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -2,24 +2,30 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         
+        // Here we will traverse the array twice but we will fill the answwr during the second pass from n-1 to 0
         int n = nums.size();
-        vector<int>ans(n, -1);
+        vector<int>ans(n);
         stack<int>st;
 
-        for(int i=0; i<2*n; i++){
+        for(int i= 2*n-1; i>=0; i--){
 
-            int num = nums[i % n];
-
-            while(!st.empty() && nums[st.top()] < num){
-                ans[st.top()] = num;
+            while(!st.empty() && st.top() <= nums[i%n]){
                 st.pop();
             }
 
+            // Filling up answers 
             if(i<n){
-                st.push(i);
+                if(st.empty()){
+                    ans[i] = -1;
+                }else{
+                    ans[i] = st.top();
+                }
             }
+
+            st.push(nums[i%n]);
         }
 
         return ans;
+
     }
 };
