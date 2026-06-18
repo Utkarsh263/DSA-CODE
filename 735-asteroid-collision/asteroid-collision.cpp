9d1e47b6bb1c451920ed9e87a5d5158805a92825
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-18 17:49:13 +0530

    Time: 3 ms (28.49%) | Memory: 22.3 MB (18.26%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,38 +1,48 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        
-        stack<int>st;
 
-        for(int a : asteroids){
+        stack<int> st;
+
+        for(int asteroid : asteroids) {
+
             bool destroyed = false;
 
-            while(!st.empty() && st.top() > 0 && a < 0){
-                if(abs(st.top()) < abs(a)){
+            while(!st.empty() &&
+                  st.top() > 0 &&
+                  asteroid < 0) {
+
+                // Current asteroid is bigger
+                if(abs(asteroid) > st.top()) {
                     st.pop();
-                }else if(abs(st.top()) == abs(a)){
+                }
+
+                // Equal size
+                else if(abs(asteroid) == st.top()) {
                     st.pop();
-                    destroyed = true;
-                    break;
-                }else{
                     destroyed = true;
                     break;
                 }
 
+                // Stack asteroid is bigger
+                else {
+                    destroyed = true;
+                    break;
+                }
             }
 
-            if(!destroyed){
-                st.push(a);
+            if(!destroyed) {
+                st.push(asteroid);
             }
         }
 
-        vector<int>result(st.size());
+        vector<int> ans(st.size());
 
-        for(int i=st.size()-1; i>=0; i--){
-            result[i] = st.top();
+        for(int i = st.size() - 1; i >= 0; i--) {
+            ans[i] = st.top();
             st.pop();
         }
 
-        return result;
+        return ans;
     }
 };
