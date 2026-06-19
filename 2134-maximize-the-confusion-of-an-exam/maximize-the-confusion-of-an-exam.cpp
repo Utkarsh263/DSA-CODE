ae6555fc28d3fd8b6f5f7aa68426dcff63dde6d7
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-19 18:23:50 +0530

    Time: 16 ms (21.86%) | Memory: 12.6 MB (55.57%) - LeetSync

---
+++ 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int solve(string &s , int k , char badChar){
+
+        int left = 0;
+        int badCount = 0;
+        int ans = 0;
+
+        for(int right = 0; right < s.size(); right++){
+
+            if(s[right] == badChar){
+                badCount++;
+            }
+
+            while(badCount > k){
+
+                if(s[left] == badChar){
+                    badCount--;
+                }
+
+                left++;
+            }
+
+            ans = max(ans , right-left+1);
+        }
+
+        return ans;
+    }
+    int maxConsecutiveAnswers(string answerKey, int k) {
+        
+        return max(
+            solve(answerKey , k , 'T'),
+            solve(answerKey , k , 'F')
+        );
+    }
+};
