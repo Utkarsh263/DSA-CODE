866f6f5aea17a8675b5bb8997b4845bf70431d47
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-21 00:30:12 +0530

    Time: 320 ms (58.23%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-Select score, 
-DENSE_RANK() over (Order by score desc) as 'rank'
-from scores
-order by score desc;
+
+SELECT score , DENSE_RANK() over (order by score desc) as 'rank' 
+from Scores;
