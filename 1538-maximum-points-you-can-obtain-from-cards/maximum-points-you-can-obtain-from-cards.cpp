8ad91a2d5365ce649cd67ac0410a65aedf10539e
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-20 10:46:26 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (33.36%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
@@ -3,13 +3,15 @@ public:
     int maxScore(vector<int>& cardPoints, int k) {
         
         int n = cardPoints.size();
-        int total =0;
+        int total = 0;
 
         for(int x : cardPoints){
             total += x;
         }
 
         int windowSize = n-k;
+
+        // Finding window sum 
         int sum = 0;
         for(int i=0; i<windowSize; i++){
             sum += cardPoints[i];
@@ -17,11 +19,12 @@ public:
 
         int minSum = sum;
 
-        for(int i=windowSize; i<n; i++){
-            sum += cardPoints[i] - cardPoints[i-windowSize];
-            minSum = min(minSum , sum);
+        for(int i= windowSize ; i<n; i++){
+
+            sum = sum + cardPoints[i] - cardPoints[i-windowSize];
+            minSum = min(sum , minSum);
         }
 
-        return total - minSum;
+        return total-minSum;
     }
 };
