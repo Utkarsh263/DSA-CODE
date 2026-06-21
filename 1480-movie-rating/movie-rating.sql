3ffd62181e6cb3ac915adcd9a3f664f4f2a149a5
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-21 22:13:04 +0530

    Time: 1520 ms (21.65%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -2,26 +2,23 @@
 
 -- Merge of 2 questions 
 
-(SELECT u.name AS results
+(SELECT u.name as results 
 FROM Users u 
 JOIN 
-MovieRating m 
-ON u.user_id = m.user_id 
-GROUP BY u.user_id , u.name
+MovieRating mr 
+ON u.user_id = mr.user_id 
+GROUP BY u.user_id, u.name 
 ORDER BY COUNT(*) DESC , u.name ASC
 LIMIT 1)
 
-UNION ALL
+UNION ALL 
 
-(SELECT m.title AS results
-FROM Movies m
+(SELECT m.title as results
+FROM Movies m 
 JOIN 
-MovieRating mr
-on m.movie_id = mr.movie_id 
-WHERE mr.created_at BETWEEN 
-        '2020-02-01' 
-        AND 
-        '2020-02-29'
+MovieRating mr 
+ON m.movie_id = mr.movie_id 
+WHERE mr.created_at BETWEEN '2020-02-01' AND '2020-02-29'
 GROUP BY m.movie_id, m.title 
-ORDER BY AVG(rating) DESC , m.title ASC
+ORDER BY AVG(mr.rating) DESC , m.title ASC
 LIMIT 1)
