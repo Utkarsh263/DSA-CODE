ca5e6a398a92c467476875468c7e9765eeb508b4
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-21 19:31:58 +0530

    Time: 594 ms (91.12%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,6 +1,22 @@
 # Write your MySQL query statement below
-DELETE P1 
-FROM person p1
-JOIN person p2
-on p1.email = p2.email 
-where p1.id > p2.id;
+ 
+--  WE can use join and Row number both to solve 
+
+-- DELETE p1 
+-- FROM Person p1 
+-- JOIN 
+-- Person p2 
+-- on p1.email = p2.email 
+-- where p1.id > p2.id;
+
+-- Using ROW_NUMBER
+
+DELETE FROM PERSON 
+WHERE id IN (
+    SELECT id 
+    FROM (
+        SELECT * , ROW_NUMBER() OVER(partition by email order by id) AS rnk
+        FROM PERSON
+    )t
+    WHERE rnk > 1
+)
