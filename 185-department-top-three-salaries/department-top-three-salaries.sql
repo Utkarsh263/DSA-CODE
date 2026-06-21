6fe1be6afc05370ad869bc6a0c8538cfb2dd6809
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-21 22:39:56 +0530

    Time: 1081 ms (39.51%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,22 +1,13 @@
--- # Write your MySQL query statement below
-
--- WITH new_table AS (
---     SELECT 
---         d.name AS Department,
---         e.name AS Employee,
---         e.salary AS Salary,
---         DENSE_RANK() OVER(PARTITION BY d.name ORDER BY e.salary DESC) AS ranking
---     FROM Employee e
---     LEFT JOIN Department d
---     ON e.departmentId = d.id
--- )
--- SELECT Department, Employee, Salary
--- FROM new_table
--- WHERE ranking <= 3;
-
-Select d.name as department , e1.name as employee, e1.salary as Salary
-From Employee e1 join Department d on e1.DepartmentId = d.Id
-Where  3 > (select count(distinct (e2.Salary))
-            from  Employee e2
-            where e2.Salary > e1.Salary
-            and e1.DepartmentId = e2.DepartmentId)
+# Write your MySQL query statement below
+SELECT Department , Employee , Salary 
+FROM (
+    SELECT d.name as Department , 
+    e.name as Employee, 
+    e.salary as Salary, 
+    DENSE_RANK() OVER (PARTITION BY d.name ORDER BY e.salary DESC) AS 'rnk'
+    FROM Employee e
+    JOIN 
+    Department d 
+    ON e.departmentId = d.id
+)t 
+WHERE rnk <= 3
