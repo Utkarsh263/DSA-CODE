af9c5a39f22ebbb8dd02d07bb592f889c4ae79d2
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-21 10:52:38 +0530

    Time: 4 ms (85.02%) | Memory: 44.7 MB (77.72%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         
-        // Sort the cookies and greed vector so that greedier child gets cookies bigger in size to get satisfied 
+        // Sort the grred and cookies vector so that the greedier chid get the cookies bigger in size to get satisfied 
 
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
 
-        int i =0;
+        int i=0;
         int j=0;
+
         int m = g.size();
         int n = s.size();
 
