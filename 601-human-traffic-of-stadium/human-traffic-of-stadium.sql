4b3e6f8243d311d687bbcf76adfe2c81f86008ca
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-21 18:06:53 +0530

    Time: 399 ms (39.13%) | Memory: 0B (100.00%) - LeetSync

--- 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
+++ 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
@@ -1,15 +1,31 @@
 # Write your MySQL query statement below
+-- WITH cte as (
+--     SELECT * , id - ROW_NUMBER() over (order by id) as 'grp'
+--     FROM Stadium
+--     where people >= 100
+-- )
+
+-- SELECT id , visit_date , people
+-- FROM cte
+-- where grp in (
+--     SELECT grp from cte
+--     group by grp 
+--     having count(*) >= 3
+-- )
+-- order by visit_date;
+
 WITH cte as (
-    SELECT * , id - ROW_NUMBER() over (order by id) as 'grp'
-    FROM Stadium
+    SELECT * , id-ROW_NUMBER() over (Order by id) as 'grp'
+    from Stadium
     where people >= 100
 )
 
-SELECT id , visit_date , people
-FROM cte
+SELECT id, visit_date , people
+from cte 
 where grp in (
-    SELECT grp from cte
-    group by grp 
+    SELECT grp 
+    from cte 
+    group by grp
     having count(*) >= 3
 )
-order by visit_date;
+Order by visit_date;
