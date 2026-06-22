c0148ed02c7a10a4e23d91da4401c437678cc3c8
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-22 22:26:16 +0530

    Time: 301 ms (18.00%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-
-SELECT * from Cinema 
-where id % 2 = 1 AND description <> 'boring'
-order by rating desc;
+SELECT * FROM Cinema
+WHERE id % 2 = 1 AND 
+description <> 'boring'
+ORDER BY rating DESC;
