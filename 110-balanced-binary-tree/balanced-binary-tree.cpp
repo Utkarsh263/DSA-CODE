2f5795923d894c1d2f8d6391bbe0718a1a930c01
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-22 17:40:10 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (20.07%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -11,30 +11,63 @@
  */
 class Solution {
 public:
+
     int height(TreeNode* root){
 
         if(root == NULL){
             return 0;
         }
 
-        int left = height(root->left);
-        int right = height(root->right);
-
-        return 1+max(left , right);
-    }
-    bool isBalanced(TreeNode* root) {
-        
-        if(root == NULL){
-            return true;
+        int leftHeight = height(root->left);
+        if(leftHeight == -1){
+            return -1;
         }
 
-        int leftHeight = height(root->left);
         int rightHeight = height(root->right);
+        if(rightHeight == -1){
+            return -1;
+        }
 
         if(abs(leftHeight - rightHeight) > 1){
-            return false;
+            return -1;
         }
 
-        return isBalanced(root->left) && isBalanced(root->right);
+        return 1 + max(leftHeight, rightHeight);
+    }
+
+    bool isBalanced(TreeNode* root) {
+
+        return height(root) != -1;
     }
 };
+
+// Brute Force approach 
+// class Solution {
+// public:
+//     int height(TreeNode* root){
+
+//         if(root == NULL){
+//             return 0;
+//         }
+
+//         int left = height(root->left);
+//         int right = height(root->right);
+
+//         return 1+max(left , right);
+//     }
+//     bool isBalanced(TreeNode* root) {
+        
+//         if(root == NULL){
+//             return true;
+//         }
+
+//         int leftHeight = height(root->left);
+//         int rightHeight = height(root->right);
+
+//         if(abs(leftHeight - rightHeight) > 1){
+//             return false;
+//         }
+
+//         return isBalanced(root->left) && isBalanced(root->right);
+//     }
+// };
