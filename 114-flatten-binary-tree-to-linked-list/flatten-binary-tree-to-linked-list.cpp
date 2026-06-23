83b1da01653aaf10acda436c992cec686726bc08
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-23 23:50:28 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (78.02%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -11,36 +11,68 @@
  */
 class Solution {
 public:
-
-    vector<TreeNode*> nodes;
-
-    void preorder(TreeNode* root) {
-
-        if(root == NULL) {
+    TreeNode* LastVisited = NULL;
+    void flatten(TreeNode* root) {
+        
+        if(root == NULL){
             return;
         }
 
-        nodes.push_back(root);
+        flatten(root->right);
+        flatten(root->left);
 
-        preorder(root->left);
-        preorder(root->right);
-    }
-
-    void flatten(TreeNode* root) {
-
-        preorder(root);
-
-        int n = nodes.size();
-
-        for(int i = 0; i < n - 1; i++) {
-
-            nodes[i]->left = NULL;
-            nodes[i]->right = nodes[i + 1];
-        }
-
-        if(n > 0) {
-            nodes[n - 1]->left = NULL;
-            nodes[n - 1]->right = NULL;
-        }
+        root->left = NULL;
+        root->right = LastVisited;
+        LastVisited = root;
     }
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// class Solution {
+// public:
+
+//     vector<TreeNode*> nodes;
+
+//     void preorder(TreeNode* root) {
+
+//         if(root == NULL) {
+//             return;
+//         }
+
+//         nodes.push_back(root);
+
+//         preorder(root->left);
+//         preorder(root->right);
+//     }
+
+//     void flatten(TreeNode* root) {
+
+//         preorder(root);
+
+//         int n = nodes.size();
+
+//         for(int i = 0; i < n - 1; i++) {
+
+//             nodes[i]->left = NULL;
+//             nodes[i]->right = nodes[i + 1];
+//         }
+
+//         if(n > 0) {
+//             nodes[n - 1]->left = NULL;
+//             nodes[n - 1]->right = NULL;
+//         }
+//     }
+// };
+
+
