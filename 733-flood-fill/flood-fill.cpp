aa7d09d49cd3ce170c196bfccf18259ba8c36941
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-24 17:57:29 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (90.88%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -2,26 +2,27 @@ class Solution {
 public:
     int m;
     int n;
-    void dfs(vector<vector<int>>&image , int i, int j, int newColor , int orgColor){
+    void dfs(vector<vector<int>>&image , int i, int j , int newColor, int orgColor){
 
-        if(i<0 || i>= m || j<0 || j>= n || image[i][j] != orgColor || image[i][j] == newColor){
+        if(i<0 || j<0 || i>=m || j>= n || image[i][j] != orgColor || image[i][j] == newColor){
             return;
         }
-
         image[i][j] = newColor;
 
-        dfs(image , i-1, j, newColor , orgColor);
         dfs(image , i+1, j, newColor , orgColor);
-        dfs(image , i, j-1, newColor , orgColor);
+        dfs(image , i-1, j, newColor , orgColor);
         dfs(image , i, j+1, newColor , orgColor);
+        dfs(image , i, j-1, newColor , orgColor);
+
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-
+        
         m = image.size();
         n = image[0].size();
-        
+
         int orgColor = image[sr][sc];
-        dfs(image, sr, sc , color , orgColor);
+
+        dfs(image, sr, sc, color, orgColor);
 
         return image;
     }
