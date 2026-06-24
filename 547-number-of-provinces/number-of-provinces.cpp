39210f900c87a7eff4e11c6ec6cfb84f60f90366
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-24 16:47:33 +0530

    Time: 6 ms (14.59%) | Memory: 21 MB (16.98%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     void dfs(int node , vector<vector<int>>&adj , vector<bool>&visited){
 
-        visited[node] = true; // Mark the node as visited 
+        visited[node] = true;
 
-        // Visit its neighbors 
         for(auto it : adj[node]){
             if(!visited[it]){
                 dfs(it, adj, visited);
@@ -13,34 +12,32 @@ public:
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
         
-        // Building the  Graph from the matrix 
         int n = isConnected.size();
         vector<vector<int>>adj(n);
 
+        // Build the Graph from the matrix 
+
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                if(i != j && isConnected[i][j] == 1){
+                if(i != j && isConnected[i][j] != 0){
                     adj[i].push_back(j);
                     adj[j].push_back(i);
                 }
             }
         }
 
-        // Building the Visited array to mark the city tracked 
+        int provinces = 0;
         vector<bool>visited(n, false);
 
-        // Traverse the Graph 
-        int provinces = 0;
-        for(int i=0; i<n; i++){
+        // Traverse the Graph and count number of provinces 
 
+        for(int i=0; i<n; i++){
             if(!visited[i]){
                 provinces++;
                 dfs(i, adj, visited);
             }
-            
         }
 
         return provinces;
-        
     }
 };
