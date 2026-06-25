1a96c11f41e495e397a67a3e4d884a71f47071e7
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-25 11:48:51 +0530

    Time: 8 ms (62.05%) | Memory: 42.6 MB (10.60%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,49 +1,56 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>&grid , int i, int j, int m , int n){
-        if(i<0 || j<0 || i>=m || j>= n ||grid[i][j] != 1){
-            return;
+    void dfs(int r, int c, vector<vector<int>>&visited , vector<vector<int>>&grid){
+
+        visited[r][c] = 1;
+        int m = grid.size();
+        int n =grid[0].size();
+
+        // Check for all neighbors 
+        if(r-1 >= 0 && !visited[r-1][c] && grid[r-1][c] == 1){
+            dfs(r-1, c, visited , grid);
         }
-        grid[i][j]=0;
 
-        dfs(grid , i-1, j,m,n);
-        dfs(grid , i+1 , j, m ,n);
-        dfs(grid , i, j-1, m ,n);
-        dfs(grid , i, j+1, m ,n);
+         if(r+1 < m && !visited[r+1][c] && grid[r+1][c] == 1){
+            dfs(r+1, c, visited , grid);
+        }
 
+         if(c-1 >= 0 && !visited[r][c-1] && grid[r][c-1] == 1){
+            dfs(r, c-1, visited , grid);
+        }
+
+         if(c+1 < n && !visited[r][c+1] && grid[r][c+1] == 1){
+            dfs(r, c+1, visited , grid);
+        }
     }
     int numEnclaves(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
 
-        // Converting boundary lands to water as 1->0
-        for(int i=0; i<m; i++){
-            if(grid[i][0]==1){
-                dfs(grid, i, 0, m, n);
-            }
-            if(grid[i][n-1]==1){
-                dfs(grid , i, n-1, m ,n);
-            }
-        }
+        vector<vector<int>>visited(m, vector<int>(n, 0));
 
-        for(int j=0; j<n; j++){
-            if(grid[0][j]==1){
-                dfs(grid ,0,j,m,n);
-            }
-            if(grid[m-1][j]==1){
-                dfs(grid,m-1,j,m,n);
-            }
-        }
-
-        int count=0;
-        // Counting the number of 1 left in the grid
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(grid[i][j]==1){
+
+                if(i==0 || i==m-1 || j==0 || j==n-1){
+
+                    if(grid[i][j] == 1 && !visited[i][j]){
+                        dfs(i, j, visited , grid);
+                    }
+                }
+            }
+        }
+
+        int count = 0;
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 1 && !visited[i][j]){
                     count++;
                 }
             }
         }
+
         return count;
     }
 };
