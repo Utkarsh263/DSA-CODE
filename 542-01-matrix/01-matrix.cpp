6abc6ef5a1b0cfff730b8cd72dd3da9ac49fa165
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-25 10:56:36 +0530

    Time: 7 ms (93.93%) | Memory: 39 MB (20.34%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,68 +1,57 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        // 286, 1765
+        
+        int m = mat.size();
+        int n = mat[0].size();
 
-        int n = mat.size();
-        int m = mat[0].size();
+        vector<vector<int>>visited(m, vector<int>(n, 0));
+        vector<vector<int>>dist(m , vector<int>(n, 0));
 
-        // Visited Matrix 
-        vector<vector<int>>vis(n, vector<int>(m, 0));
-        // Distance Matrix
-        vector<vector<int>>dist(n, vector<int>(m ,0));
-
-        // Queue for bfs traversal (i, j) , dist
         queue<pair<pair<int, int>, int>>q;
 
-        // First push all 0's in the queue with dist 0 for multi-source bfs 
+        // Push all 0's into the queue
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(mat[i][j] == 0 && !visited[i][j]){
+                    q.push({{i, j}, 0});
+                    visited[i][j] = 1;
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(mat[i][j] == 0){
-                    q.push({{i, j} , 0});
-                    vis[i][j] = 1;
                 }
             }
         }
 
-        // Perform bfs 
-
+        // Process the queue 
         while(!q.empty()){
 
             int r = q.front().first.first;
             int c = q.front().first.second;
             int d = q.front().second;
-
+            dist[r][c] = d;
             q.pop();
 
-            dist[r][c] = d; // Stored the distance of the nodes being visited
-
-            // Explore all 4 directions 
-            
-             // UP
-            if(r-1 >= 0 && vis[r-1][c] == 0) {
-                q.push({{r-1, c}, d+1});
-                vis[r-1][c] = 1;
+            if(r-1 >= 0 && !visited[r-1][c]) {
+                visited[r-1][c] = 1;
+                q.push({{r-1,c}, d+1});
             }
 
-            // DOWN
-            if(r+1 < n && vis[r+1][c] == 0) {
-                q.push({{r+1, c}, d+1});
-                vis[r+1][c] = 1;
+            // Down
+            if(r+1 < m && !visited[r+1][c]) {
+                visited[r+1][c] = 1;
+                q.push({{r+1,c}, d+1});
             }
 
-            // LEFT
-            if(c-1 >= 0 && vis[r][c-1] == 0) {
-                q.push({{r, c-1}, d+1});
-                vis[r][c-1] = 1;
+            // Left
+            if(c-1 >= 0 && !visited[r][c-1]) {
+                visited[r][c-1] = 1;
+                q.push({{r,c-1}, d+1});
             }
 
-            // RIGHT
-            if(c+1 < m && vis[r][c+1] == 0) {
-                q.push({{r, c+1}, d+1});
-                vis[r][c+1] = 1;
+            // Right
+            if(c+1 < n && !visited[r][c+1]) {
+                visited[r][c+1] = 1;
+                q.push({{r,c+1}, d+1});
             }
-
         }
 
         return dist;
