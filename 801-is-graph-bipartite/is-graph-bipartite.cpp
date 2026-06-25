8ad985db468903bd0f60a1788ae4d52007fc20f5
Utkarsh Kohli <utkarshkohli821@gmail.com>
2026-06-25 13:43:53 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (59.37%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -5,10 +5,12 @@ public:
         colors[node] = color;
 
         for(int neighbour : graph[node]){
-            if(colors[neighbour] == -1){ 
-                if(!dfs(neighbour ,1- color, graph , colors)){
+
+            if(colors[neighbour] == -1){
+
+               if(!dfs(neighbour , 1-color , graph , colors)){
                     return false;
-                }
+               }
             }else if(colors[neighbour] == color){
                 return false;
             }
@@ -19,13 +21,10 @@ public:
     bool isBipartite(vector<vector<int>>& graph) {
         
         int n = graph.size();
-        vector<int>colors(n , -1);
+        vector<int>colors(n, -1);
 
-        // Traverse the grph dfs 
         for(int i=0; i<n; i++){
-
-            if(colors[i] == -1){ // Call for dfs for the uncolored node and start coloring 
-                
+            if(colors[i] == -1){
                 if(!dfs(i, 0, graph , colors)){
                     return false;
                 }
